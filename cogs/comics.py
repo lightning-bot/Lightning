1140f0584b97b24b4ddb058182aaf9f2aff6920a
UmbraSage <umbrasage@users.noreply.github.com>
2019-04-11 20:53:56 -0500

    Add More Comics
    cuz why not

--- cogs/comics.py
+++ cogs/comics.py
@@ -64,7 +64,7 @@ class Comics(Cog):
 
     @commands.command(aliases=['rgc'])
     @commands.bot_has_permissions(embed_links=True)
-    async def randomgarfieldcomic(self, ctx):
+    async def random_garfieldcomic(self, ctx):
         """Displays a random garfield comic
         
         Powered by GoComics"""
@@ -80,7 +80,7 @@ class Comics(Cog):
         await ctx.send(embed=embed)
 
     @commands.command(aliases=['ruac'])
-    async def randomusacrescomic(self, ctx):
+    async def random_usacrescomic(self, ctx):
         """Displays a random US Acres comic
         
         Powered by GoComics"""
@@ -90,11 +90,58 @@ class Comics(Cog):
 
         img_url = soup.find(attrs={'class':'item-comic-image'}).img['src']
 
-        embed = discord.Embed(title="Random U.S. Acres Comic")
+        embed = discord.Embed(title="Random U.S. Acres Comic", color=discord.Color(0xC3E4F7))
         embed.set_image(url=img_url)
         embed.set_footer(text=f'Requested by {ctx.author}', icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
+    @commands.command(aliases=['rpc'])
+    async def random_peanutscomic(self, ctx):
+        """Displays a random Peanuts comic
+        
+        Powered by GoComics"""
+        url = "https://www.gocomics.com/random/peanuts"
+        async with self.session.get(url) as response:
+            soup = BeautifulSoup(await response.text(), "html.parser")
+
+        img_url = soup.find(attrs={'class':'item-comic-image'}).img['src']
+
+        embed = discord.Embed(title="Random Peanuts Comic", color=discord.Color(0xFE0000))
+        embed.set_image(url=img_url)
+        embed.set_footer(text=f'Requested by {ctx.author}', icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases=['rgmgc'])
+    async def random_garfieldminusgarfieldcomic(self, ctx):
+        """Displays a random Garfield Minus Garfield comic
+        
+        Powered by GoComics"""
+        url = "https://www.gocomics.com/random/garfieldminusgarfield"
+        async with self.session.get(url) as response:
+            soup = BeautifulSoup(await response.text(), "html.parser")
+
+        img_url = soup.find(attrs={'class':'item-comic-image'}).img['src']
+
+        embed = discord.Embed(title="Random Garfield Minus Garfield Comic", color=discord.Color(0xFF9900))
+        embed.set_image(url=img_url)
+        embed.set_footer(text=f'Requested by {ctx.author}', icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases=['rnsc'])
+    async def random_nonsequiturcomic(self, ctx):
+        """Displays a random Non Sequitur comic
+        
+        Powered by GoComics"""
+        url = "https://www.gocomics.com/random/nonsequitur"
+        async with self.session.get(url) as response:
+            soup = BeautifulSoup(await response.text(), "html.parser")
+
+        img_url = soup.find(attrs={'class':'item-comic-image'}).img['src']
+
+        embed = discord.Embed(title="Random Non Sequitur Comic", color=discord.Color(0xE9F8FF))
+        embed.set_image(url=img_url)
+        embed.set_footer(text=f'Requested by {ctx.author}', icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
 
 
 
