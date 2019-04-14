abbc155eb890783234f45fce41e232cedcb611af
UmbraSage <umbrasage@users.noreply.github.com>
2019-04-14 18:12:30 -0500

    Logs Cog Management Commands

--- cogs/load.py
+++ cogs/load.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import config
 
 
 class Load(commands.Cog, name='Cog Management'):
@@ -14,12 +15,13 @@ class Load(commands.Cog, name='Cog Management'):
     async def c_load(self, ctx, *, cog: str):
         """Load a Cog.
         e.g: owner"""
-
+        log_channel = self.bot.get_channel(config.error_channel)
         try:
             self.bot.load_extension("cogs." + cog)
         except Exception as e:
             await ctx.send(f'💢 There was an error loading the cog \n**`ERROR:`** {type(e).__name__} - {e}')
         else:
+            await log_channel.send(f"{ctx.author.mention} loaded the cog `{cog}`")
             await ctx.send('✅ Successfully Loaded Cog')
 
     @commands.command(name='unload', hidden=True)
@@ -27,12 +29,14 @@ class Load(commands.Cog, name='Cog Management'):
     async def c_unload(self, ctx, *, cog: str):
         """Unloads a Cog.
         e.g: owner"""
+        log_channel = self.bot.get_channel(config.error_channel)
 
         try:
             self.bot.unload_extension("cogs." + cog)
         except Exception as e:
             await ctx.send(f'💢 There was an error unloading the cog \n***`ERROR:`** {type(e).__name__} - {e}')
         else:
+            await log_channel.send(f"{ctx.author.mention} unloaded the cog `{cog}`")
             await ctx.send('✅ Successfully unloaded cog')
 
     @commands.command(name='reload', hidden=True)
@@ -40,6 +44,7 @@ class Load(commands.Cog, name='Cog Management'):
     async def c_reload(self, ctx, *, cog: str):
         """Reload a Cog.
         e.g: owner"""
+        log_channel = self.bot.get_channel(config.error_channel)
 
         try:
             self.bot.unload_extension("cogs." + cog)
@@ -47,40 +52,50 @@ class Load(commands.Cog, name='Cog Management'):
         except Exception as e:
             await ctx.send(f'💢 There was an error reloading the cog \n**`ERROR:`** {type(e).__name__} - {e}')
         else:
+            log_channel.send(f"{ctx.author.mention} reloaded the cog `{cog}`")
             await ctx.send('✅ Successfully reloaded cog')
 
     @commands.command(name='addonload', aliases=["addload"])
     @commands.is_owner()
     async def addon_load(self, ctx, *, addon: str):
         """Load an addon from the Addons folder"""
+        log_channel = self.bot.get_channel(config.error_channel)
+        
         try:
             self.bot.load_extension("addons." + addon)
         except Exception as e:
             await ctx.send(f'💢 There was an error loading the addon \n**`ERROR:`** {type(e).__name__} - {e}')
         else:
+            await log_channel.send(f"{ctx.author.mention} loaded addon `{addon}`")
             await ctx.send(f'✅ Successfully loaded addon `addons.{addon}`')
 
     @commands.command(name='addonunload', aliases=["addunload"])
     @commands.is_owner()
     async def addon_unload(self, ctx, *, addon: str):
         """Unload an addon from the Addons folder"""
+        log_channel = self.bot.get_channel(config.error_channel)
+
         try:
             self.bot.unload_extension("addons." + addon)
         except Exception as e:
             await ctx.send(f'💢 There was an error unloading the addon \n**`ERROR:`** {type(e).__name__} - {e}')
         else:
+            await log_channel.send(f"{ctx.author.mention} unloaded addon `{addon}`")
             await ctx.send(f'✅ Successfully unloaded addon `addons.{addon}`')
     
     @commands.command(name='addonreload', aliases=["addreload"])
     @commands.is_owner()
     async def addon_reload(self, ctx, *, addon: str):
         """Reload an addon from the Addons folder"""
+        log_channel = self.bot.get_channel(config.error_channel)
+
         try:
             self.bot.unload_extension("addons." + addon)
             self.bot.load_extension("addons." + addon)
         except Exception as e:
             await ctx.send(f'💢 There was an error reloading the addon \n**`ERROR:`** {type(e).__name__} - {e}')
         else:
+            await log_channel.send(f"{ctx.author.mention} reloaded addon `{addon}`")
             await ctx.send(f'✅ Successfully reloaded addon `addons.{addon}`')
     
 
