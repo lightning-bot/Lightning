d7a15fc7e562bd4227f737f10ef2f8de8d33ba0e
UmbraSage <umbrasage@users.noreply.github.com>
2019-04-13 22:40:23 -0500

    Add descripts. and .dog

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,11 +12,22 @@ class Fun(commands.Cog):
 
     @commands.command()
     async def cat(self, ctx):
+        """Random Cat Pics from random.cat"""
         async with self.session.get('http://aws.random.cat/meow') as resp:
             data = await resp.json()
         embed = discord.Embed(title="Meow <:meowawauu:559383939513581569>", color=discord.Color.teal())
         embed.set_image(url=data['file'])
         embed.set_footer(text="Powered by random.cat", icon_url="https://purr.objects-us-east-1.dream.io/static/ico/favicon-96x96.png")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def dog(self, ctx):
+        """Random dog pics from dog.ceo"""
+        async with self.session.get('https://dog.ceo/api/breeds/image/random') as resp:
+            data = await resp.json()
+        embed = discord.Embed(title="Bark 🐶", color=discord.Color.blurple())
+        embed.set_image(url=data['message'])
+        embed.set_footer(text="Powered by dog.ceo", icon_url="https://dog.ceo/img/favicon.png")
         await ctx.send(embed=embed)
 
 
