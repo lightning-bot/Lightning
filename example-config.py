3f772306cd0cad1ca57faab1b607719bba85ebe8
UmbraSage <umbrasage@users.noreply.github.com>
2019-04-14 17:30:55 -0500

    Add Cog List and Error Channel

--- example-config.py
+++ example-config.py
@@ -1,3 +1,10 @@
 # Basic Configuration
 token = "insert-token-here"
 description  = "Lightning+, the successor to Lightning(.js)"
+
+# List Cogs that should be loaded on start
+cogs = ['cogs.comics', 'cogs.owner', 'cogs.load', 'cogs.moderation', 'cogs.logger', 'cogs.extras', 'cogs.role',
+'cogs.mod_userlog', 'cogs.setup', 'cogs.weeb', 'cogs.info', 'cogs.note', 'cogs.fun', 'db.databasestart']
+
+# Bot Errors Channel. 
+error_channel = 567109464184979502
