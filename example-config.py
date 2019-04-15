e8ec0a02e7c2fd153a29a6244ba10f5ac7852506
UmbraSage <umbrasage@users.noreply.github.com>
2019-04-15 16:56:14 -0500

    Update Config Cogs List

--- example-config.py
+++ example-config.py
@@ -4,7 +4,6 @@ description  = "Lightning+, the successor to Lightning(.js)"
 
 # List Cogs that should be loaded on start
 cogs = ['cogs.comics', 'cogs.owner', 'cogs.load', 'cogs.moderation', 'cogs.logger', 'cogs.extras', 'cogs.role',
-'cogs.mod_userlog', 'cogs.setup', 'cogs.weeb', 'cogs.info', 'cogs.note', 'cogs.fun', 'db.databasestart']
-
+'cogs.mod_userlog', 'cogs.setup', 'cogs.weeb', 'cogs.info', 'cogs.mod_note', 'cogs.fun', 'db.databasestart', 'cogs.toggle_roles']
 # Bot Errors Channel. 
 error_channel = 567109464184979502
