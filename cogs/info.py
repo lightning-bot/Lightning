cf7f0c6550206e9780f9ba3185021e6d24c6538a
UmbraSage <umbrasage@users.noreply.github.com>
2019-04-14 22:28:21 -0500

    More Changes
    -Support for Guild Join/Leave Logging
    -Updated invite link
    -Updated welcome-and-rules channel
    -Support for checking the latest commits in the about embed

--- cogs/info.py
+++ cogs/info.py
@@ -2,6 +2,8 @@ import discord
 import aiohttp
 import datetime
 import time
+import os
+import config
 from discord.ext import commands
 
 
@@ -16,18 +18,30 @@ class Info(commands.Cog):
     async def supportserver(self, ctx):
         """Gives an invite to the support server"""
         if isinstance(ctx.channel, discord.DMChannel) or ctx.guild.id != 527887739178188830:
-            return await ctx.send(f"**Here you go {ctx.author.name} \n<https://discord.gg/ZqU3XEw>**")
+            return await ctx.send(f"**Here you go {ctx.author.name} \n<https://discord.gg/cDPGuYd>**")
 
-        await ctx.send(f"**{ctx.author}** You're asking for an invite in the support server? <:blobthonk:537791813990350873>\n~~There's an invite in <#527887739178188834> btw~~")
+        await ctx.send(f"{ctx.author.mention} You're asking for an invite in the support server? <:blobthonk:537791813990350873>\n~~There's an invite in <#567138592208453635> btw~~")
 
     @commands.command()
     async def about(self, ctx):
         """About Lightning+"""
+        #Snippet of code taken from RoboDanny. https://github.com/Rapptz/RoboDanny/blob/fb9c470b48e0333c58872d319cdbb9a42ec887c7/cogs/stats.py
+        cmd = r'git show -s HEAD~3..HEAD --format="[{}](https://github.com/UmbraSage/Lightning.py/commit/%H) %s (%cr)"'
+        if os.name == 'posix':
+            cmd = cmd.format(r'\`%h\`')
+        else:
+            cmd = cmd.format(r'`%h`')
+
+        try:
+            revision = os.popen(cmd).read().strip()
+        except OSError:
+            revision = 'Could not fetch due to memory error. Sorry. :('
         embed = discord.Embed(title="Lightning+")
         embed.set_author(name="UmbraSage#7867")
         embed.set_thumbnail(url="https://assets.gitlab-static.net/uploads/-/system/user/avatar/3717366/avatar.png?width=90")
         embed.url = "https://github.com/UmbraSage/Lightning.py"
         embed.description = "Lightning+, the successor to Lightning(.js)."
+        embed.add_field(name="Latest Changes:", value=revision)
         await ctx.send(embed=embed)
 
 
@@ -68,6 +82,29 @@ class Info(commands.Cog):
             number += ms
         average = round(number / len(pings))
         await ctx.send(f"__**Ping Times:**__\nTyping: `{typingms}ms`  |  Latency: `{latencyms}ms`\nDiscord: `{discordms}`  |  Average: `{average}ms`")
+
+
+    async def send_guild_info(self, embed, guild):
+        embed.add_field(name='Guild Name', value=guild.name)
+        embed.add_field(name='Guild ID', value=guild.id)
+        member_count = guild.member_count
+        embed.add_field(name='Member Count', value=str(member_count))
+        embed.add_field(name='Owner', value=f"{guild.owner} | ID: {guild.owner.id}")
+        log_channel = self.bot.get_channel(config.error_channel)
+        await log_channel.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        embed = discord.Embed(title="Joined New Guild", color=discord.Color.blue())
+        await self.send_guild_info(embed, guild)
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        embed = discord.Embed(title="Left Guild", color=discord.Color.red())
+        await self.send_guild_info(embed, guild)
+
+
+
 
 
 
