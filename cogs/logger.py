9ac955bf60122d749cb8cb1b201d854bd7664344
UmbraSage <umbrasage@users.noreply.github.com>
2019-04-14 22:45:35 -0500

    Moderation: Part 1
    -Added ModNote Cog
    under the MIT License https://github.com/reswitched/robocop-ng/blob/master/cogs/mod_note.py
    
    -Added Logger Cog (tracks member changes)

---
+++ cogs/logger.py
@@ -0,0 +1,77 @@
+import discord
+from discord.ext.commands import Cog
+import db.per_guild_config
+import datetime
+
+class Logger(Cog):
+    """Logs user actions"""
+    def __init__(self, bot):
+        self.bot = bot
+        print(f'Cog "{self.qualified_name}" loaded')
+
+    # Snippet of code from Noirscape's kirigiri. Under the AGPL 3.0 License, https://git.catgirlsin.space/noirscape/kirigiri/src/branch/master/LICENSE
+    async def cog_before_invoke(self, ctx):
+        if db.per_guild_config.exist_guild_config(ctx.guild, "config"):
+            ctx.guild_config = db.per_guild_config.get_guild_config(ctx.guild, "config")
+        else:
+            ctx.guild_config = {}
+
+    async def cog_after_invoke(self, ctx):
+        db.per_guild_config.write_guild_config(ctx.guild, ctx.guild_config, "config")
+
+
+    @Cog.listener()
+    async def on_member_join(self, member):
+        await self.bot.wait_until_ready()
+        if db.per_guild_config.exist_guild_config(member.guild, "config"):
+            config = db.per_guild_config.get_guild_config(member.guild, "config")
+            if "join_log_channel" in config:
+                msg = f"<a:blobjoin:563875361402912797> **Join**: {member.mention} | "\
+                 f"{member}\n"\
+                 f"🗓 __Account Creation__: {member.created_at}\n"\
+                 f"🕓 Account Age: {member.joined_at}\n"\
+                f"🏷 __User ID__: {member.id}"
+                await self.bot.get_channel(config["join_log_channel"]).send(msg)
+
+    @Cog.listener()
+    async def on_member_remove(self, member):
+        await self.bot.wait_until_ready()
+        if db.per_guild_config.exist_guild_config(member.guild, "config"):
+            config = db.per_guild_config.get_guild_config(member.guild, "config")
+            if "join_log_channel" in config:
+                msg = f"<a:blobleave:566329423595438122> **Leave**: {member.mention} | "\
+                    f"{member}\n"\
+                    f"📅 Left Date: {datetime.datetime.utcnow()}\n"
+                f"🏷 __User ID__: {member.id}"
+            await self.bot.get_channel(config["join_log_channel"]).send(msg)
+
+    @Cog.listener()
+    async def on_guild_ban(self, guild, user):
+        if db.per_guild_config.exist_guild_config(guild, "config"):
+            config = db.per_guild_config.get_guild_config(guild, "config")
+            if "event_channel" in config:
+                message = f"🔨 **Ban**: {user.mention} | {user} (ID: {user.id})"
+                try:
+                    await self.bot.get_channel(config["event_channel"]).send(message)
+                except:
+                    pass
+
+    @Cog.listener()
+    async def on_member_unban(self, guild, user):
+        await self.bot.wait_until_ready()
+        if db.per_guild_config.exist_guild_config(guild, "config"):
+            config = db.per_guild_config.get_guild_config(guild, "config")
+            if "event_channel" in config:
+                message = f"⚠ **Unban**: {user.mention} | {user} (ID: {user.id})."
+                try:
+                    await self.bot.get_channel(config["event_channel"]).send(message)
+                except:
+                    pass
+
+
+        
+
+
+
+def setup(bot):
+    bot.add_cog(Logger(bot))
---
+++ cogs/mod_note.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Cog
+from db.user_log import userlog
+import db.mod_check
+
+# Robocop-ng's Note Cog, but slightly adapted. Robocop-ng's note cog is under the MIT License.
+# https://github.com/reswitched/robocop-ng/blob/master/cogs/mod_note.py
+class ModNote(Cog):
+    def __init__(self, bot):
+        """Note cog for adding a note to user. 
+        Useful for not taking moderation action on a user."""
+        self.bot = bot
+        print(f'Cog "{self.qualified_name}" loaded')
+
+    @commands.guild_only()
+    @db.mod_check.check_if_at_least_has_staff_role("Helper")
+    @commands.command(aliases=["addnote"])
+    async def note(self, ctx, target: discord.Member, *, note: str = ""):
+        """Adds a note to a user, staff only."""
+        userlog(ctx.guild, target.id, ctx.author, note,
+                "notes", target.name)
+        await ctx.send(f"{ctx.author.mention}: noted {target}!")
+
+    @commands.guild_only()
+    @db.mod_check.check_if_at_least_has_staff_role("Helper")
+    @commands.command(aliases=["addnoteid"])
+    async def noteid(self, ctx, target: int, *, note: str = ""):
+        """Adds a note to a user by userid, staff only."""
+        userlog(ctx.guild, target, ctx.author, note,
+                "notes")
+        await ctx.send(f"ID {target} was noted!")
+
+
+def setup(bot):
+    bot.add_cog(ModNote(bot))
