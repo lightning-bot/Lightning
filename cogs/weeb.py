a66dc4e13c279813fa89fee185cccc5269363325
UmbraSage <umbrasage@users.noreply.github.com>
2019-04-17 04:25:21 -0500

    Add slap (bc apparently I didn't add it)
    didn't commit for some reason

--- cogs/weeb.py
+++ cogs/weeb.py
@@ -24,6 +24,31 @@ class Weeb(commands.Cog):
         embed.set_footer(text="Powered by nekos.life", icon_url="https://nekos.life/static/icons/favicon-194x194.png")
         await ctx.send(embed=embed)
 
+    @commands.command(name='slap')
+    async def slap(self, ctx, person):
+        """Slap yourself or someone."""
+        async with self.session.get("https://nekos.life/api/v2/img/slap") as resp:
+            slap = await resp.json()
+        url = slap["url"]
+        color_random = [int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1)]
+        embed = discord.Embed(colour=discord.Color.from_rgb(*color_random))
+        embed.set_image(url=url)
+        embed.set_footer(text="Powered by nekos.life", icon_url="https://nekos.life/static/icons/favicon-194x194.png")
+        try:
+            person = await (commands.MemberConverter()).convert(ctx=ctx, argument=person) # Reimplementation of NekoBot/Kawaii Bot slap function. May change it in the near future.
+        except commands.BadArgument:
+            pass
+        if isinstance(person, discord.Member) and person.id == ctx.author.id:
+            embed.title = (f"*{ctx.author.name} slapped themself.*")
+        else:
+            if isinstance(person, discord.Member):
+                name = person.name
+            else:
+                name = person[:30]
+            embed.title = (f"*{ctx.author.name} slapped {name}*")
+        await ctx.send(embed=embed)
+
+    
     @commands.command()
     async def tuturu(self, ctx):
         """tuturu!"""
