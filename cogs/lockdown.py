d7e443a28aaef8bac0f0b943e16f0af50d39028d
UmbraSage <umbrasage@users.noreply.github.com>
2019-05-01 11:21:37 -0500

    Add Hard Lockdown
    -Useful for archival purposes or for actual spamming

--- cogs/lockdown.py
+++ cogs/lockdown.py
@@ -50,6 +50,35 @@ class Lockdown(commands.Cog):
 
     @commands.guild_only()
     @commands.bot_has_permissions(manage_channels=True)
+    @db.mod_check.check_if_at_least_has_staff_role("Admin")
+    @commands.command(aliases=['hard-lock'])
+    async def hlock(self, ctx, channel: discord.TextChannel = None):
+        """Hard locks a channel. Sets the channel permissions as @everyone can't speak or see the channel.
+        
+        If no channel was mentioned, it hard locks the channel the command was used in."""
+        if not channel:
+            channel = ctx.channel
+
+        if channel.overwrites_for(ctx.guild.default_role).read_messages is False:
+            await ctx.send(f"🔒 {channel.mention} is already hard locked. Use `{ctx.prefix}hard-unlock` to unlock the channel.")
+            return
+
+        await channel.set_permissions(ctx.guild.default_role, read_messages=False)
+        await channel.send(f"🔒 {channel.mention} is now hard locked.")
+
+        # Define Safe Name so we don't mess this up (again)
+        safe_name = await commands.clean_content().convert(ctx, str(ctx.author))
+        log_message = f"🔒 **Hard Lockdown**: {ctx.channel.mention} by {ctx.author.mention} | {safe_name}"
+
+        if "log_channel" in ctx.guild_config:
+            try:
+                log_channel = self.bot.get_channel(ctx.guild_config["log_channel"])
+                await log_channel.send(log_message)
+            except:
+                pass
+
+    @commands.guild_only()
+    @commands.bot_has_permissions(manage_channels=True)
     @db.mod_check.check_if_at_least_has_staff_role("Moderator")
     @commands.command()
     async def unlock(self, ctx, channel: discord.TextChannel = None):
@@ -69,6 +98,35 @@ class Lockdown(commands.Cog):
         # Define Safe Name so we don't mess this up (again)
         safe_name = await commands.clean_content().convert(ctx, str(ctx.author))
         log_message = f"🔓 **Unlock**: {ctx.channel.mention} by {ctx.author.mention} | {safe_name}"
+
+        if "log_channel" in ctx.guild_config:
+            try:
+                log_channel = self.bot.get_channel(ctx.guild_config["log_channel"])
+                await log_channel.send(log_message)
+            except:
+                pass
+
+    @commands.guild_only()
+    @commands.bot_has_permissions(manage_channels=True)
+    @db.mod_check.check_if_at_least_has_staff_role("Admin")
+    @commands.command(aliases=['hard-unlock'])
+    async def hunlock(self, ctx, channel: discord.TextChannel = None):
+        """Hard unlocks the channel mentioned. Admin only. 
+
+        If no channel was mentioned, it unlocks the channel the command was used in"""
+        if not channel:
+            channel = ctx.channel
+
+        if channel.overwrites_for(ctx.guild.default_role).read_messages is None:
+            await ctx.send(f"🔓 {channel.mention} is already unlocked.")
+            return 
+
+        await channel.set_permissions(ctx.guild.default_role, read_messages=None)
+        await channel.send(f"🔓 {channel.mention} is now unlocked.")
+
+        # Define Safe Name so we don't mess this up (again)
+        safe_name = await commands.clean_content().convert(ctx, str(ctx.author))
+        log_message = f"🔓 **Hard Unlock**: {ctx.channel.mention} by {ctx.author.mention} | {safe_name}"
 
         if "log_channel" in ctx.guild_config:
             try:
