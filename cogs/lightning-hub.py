d5d1ab233fee33b381b9684f3e9f3e6da14cab49
LightSage <lightsage01@gmail.com>
2019-06-13 22:50:24 -0500

    Add autoprobate command and add listener

--- cogs/lightning-hub.py
+++ cogs/lightning-hub.py
@@ -1,11 +1,23 @@
 from discord.ext import commands
 import discord
+import db.per_guild_config
+import db.mod_check
 
 class LightningHub(commands.Cog):
     """Helper commands for Lightning Hub only."""
     def __init__(self, bot):
         self.bot = bot
         self.bot.log.info(f'{self.qualified_name} loaded')
+
+    # Snippet of Code taken from Noirscape's kirigiri. https://git.catgirlsin.space/noirscape/kirigiri/src/branch/master/LICENSE
+    async def cog_before_invoke(self, ctx):
+        if db.per_guild_config.exist_guild_config(ctx.guild, "config"):
+            ctx.guild_config = db.per_guild_config.get_guild_config(ctx.guild, "config")
+        else:
+            ctx.guild_config = {}
+
+    async def cog_after_invoke(self, ctx):
+        db.per_guild_config.write_guild_config(ctx.guild, ctx.guild_config, "config")
 
     @commands.command(hidden=True)
     @commands.has_any_role("Trusted", "Verified")
@@ -81,9 +93,44 @@ class LightningHub(commands.Cog):
         await mod_log_chan.send(msg)
         await ctx.send(f"{target.mention} is now unprobated.")
 
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await self.bot.wait_until_ready()
+        if member.guild.id != 527887739178188830:
+            return
+        if db.per_guild_config.exist_guild_config(member.guild, "config"):
+            config = db.per_guild_config.get_guild_config(member.guild, "config")
+            if "auto_probate" in config:
+                role = discord.Object(id=546379342943617025)
+                await target.add_roles(role, reason="Auto Probate")
+                dm_message = f"You were automatically probated. Please read the rules for this server and speak in the probation channel when you are ready."
+                msg = f"**Auto Probate:** {member.mention}"
+                try:
+                    await member.send(dm_message)
+                except discord.errors.Forbidden:
+                    msg += "\nUnable to deliver message in DMs"
+                mod_log_chan = self.bot.get_channel(552583376566091805)
+                await mod_log_chan.send(msg)
 
+    @commands.command(hidden=True)
+    @commands.guild_only()
+    @db.mod_check.check_if_at_least_has_staff_role("Moderator")
+    async def autoprobate(self, ctx, status: str):
+        """Turns on or off auto probate. 
+        Use "disable" to disable auto probate. Use "enable" to enable auto probate """
+        if isinstance(ctx.channel, discord.DMChannel) or ctx.guild.id != 527887739178188830:
+            return
 
-
+        if status == "disable":
+            ctx.guild_config.pop("auto_probate")
+            await ctx.send("Auto Probate is now disabled.")
+        else:
+            return await ctx.send_help(ctx.command)
+        if status == "enable":
+            ctx.guild_config["auto_probate"] = 527887739178188830
+            await ctx.send("Auto Probate is now enabled")
+        else:
+            return await ctx.send_help(ctx.command)
 
 
 
