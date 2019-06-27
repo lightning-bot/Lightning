6876f2478995b8558cfef15db07e993eb5299d84
LightSage <lightsage01@gmail.com>
2019-06-27 11:38:12 -0500

    Fix Embed

--- cogs/toggle_roles.py
+++ cogs/toggle_roles.py
@@ -22,7 +22,7 @@ class ToggleRoles(commands.Cog):
             embed.description = ""
             role_list.append(role)
             for s in role_list:
-                embed.description += f"{s.mention}\n | Role ID {s.id}"
+                embed.description += f"{s.mention} | Role ID {s.id}\n"
         embed.set_footer(text=f"{ctx.guild.name}")
         session.close()
         await ctx.send(embed=embed)
