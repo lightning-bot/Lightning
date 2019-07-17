70c485a32197b0f7726f4850c1c7b6a3db0156a7
LightSage <lightsage01@gmail.com>
2019-07-17 10:00:32 -0500

    Update cog list

--- example-config.py
+++ example-config.py
@@ -3,8 +3,11 @@ token = "insert-token-here"
 description  = "Lightning+, the successor to Lightning(.js)"
 
 # List Cogs that should be loaded on start
-cogs = ['cogs.comics', 'cogs.owner', 'cogs.load', 'cogs.moderation', 'cogs.logger', 'cogs.extras', 'cogs.role',
-'cogs.mod_userlog', 'cogs.setup', 'cogs.weeb', 'cogs.info', 'cogs.mod_note', 'cogs.fun', 'db.databasestart', 'cogs.toggle_roles', 'cogs.common', 'cogs.status_switch']
+cogs = ['cogs.comics', 'cogs.owner', 'cogs.load', 'cogs.moderation', 'cogs.logger', 'cogs.extras', 'cogs.mod_userlog', 'cogs.setup',
+'cogs.weeb', 'cogs.info', 'cogs.mod_note', 'cogs.fun', 'db.databasestart', 'cogs.toggle_roles', 'cogs.lockdown',
+'cogs.memes', 'cogs.poll', 'cogs.common', 
+'cogs.misc', 'cogs.status_switch', 'cogs.lightning-hub', 
+'stabilite.stabilite', 'cogs.timers']
 
 # Bot Errors/Logs Channel. 
 error_channel = 567109464184979502
