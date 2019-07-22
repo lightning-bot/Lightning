3e4f429c898d90fe05ae7a0871d712bdf06dc97a
LightSage <lightsage01@gmail.com>
2019-07-22 13:28:02 -0500

    Add Github Command Support
    Squashed commit of the following:
    
    commit 3929038107425739087185a56bcbee337257013d
    Author: LightSage <lightsage01@gmail.com>
    Date:   Mon Jul 22 13:22:59 2019 -0500
    
        Commands: Replace issuestats name
    
    commit 32465d42b716a0413df6958ed733aa80eaf1ace4
    Author: LightSage <lightsage01@gmail.com>
    Date:   Mon Jul 22 11:38:55 2019 -0500
    
        Add Whitelisted Guilds for gh commands
    
    commit b6a3ea2c2a761be50cf0aafcf1d34336dbc0ed8a
    Author: LightSage <lightsage01@gmail.com>
    Date:   Mon Jul 22 09:44:28 2019 -0500
    
        Update The Example Config
    
    commit 403b7209d108791d2d036fc950a2ec8992f6e5d1
    Author: LightSage <lightsage01@gmail.com>
    Date:   Sun Jul 21 16:59:11 2019 -0500
    
        Add Exception Handling to GH Commands
    
    commit ff20afdf85c14b4d2240ef806c791fc972f4704a
    Author: LightSage <lightsage01@gmail.com>
    Date:   Thu Jul 18 17:04:37 2019 -0500
    
        #
    
    commit b2d4308443a7d58c53be149d4e2d673776bc585d
    Author: UmbraSage <umbrasage@zoho.com>
    Date:   Thu Jul 18 16:38:38 2019 -0500
    
        Fixing color on embed
    
    commit ee2cffd6a99363a7fc81a09b14e43469b1071da6
    Author: UmbraSage <umbrasage@zoho.com>
    Date:   Thu Jul 18 14:23:54 2019 -0500
    
        Missing f string
    
    commit 0a15835c532206ebf099d0ff60d8ec2425747993
    Author: LightSage <lightsage01@gmail.com>
    Date:   Thu Jul 18 08:07:12 2019 -0500
    
        Add an alias for github, archivepins, and small fixes
    
    commit 0bd4273552f1ddca75be05fb58d897b13fcca991
    Merge: 4f6cc76 5df9a05
    Author: TwilightSage <lightsage01@gmail.com>
    Date:   Wed Jul 17 19:49:48 2019 -0500
    
        Merge branch 'master' into github
    
    commit 4f6cc76b2218e8022dff0fff349c722902c873a4
    Author: TwilightSage <lightsage01@gmail.com>
    Date:   Wed Jul 17 12:17:04 2019 -0500
    
        (
    
    commit b6e6e0a20b88732938bf52d3daadea986e1cc3f5
    Author: LightSage <lightsage01@gmail.com>
    Date:   Wed Jul 17 11:49:18 2019 -0500
    
        Add Initial Github Commands
    
    commit b997d4aeeadcd176a38218bbf74f4cda4dff1e37
    Author: LightSage <lightsage01@gmail.com>
    Date:   Wed Jul 17 10:07:34 2019 -0500
    
        Add New Fields to example config for usage of the github commands and update requirements

--- example-config.py
+++ example-config.py
@@ -3,17 +3,28 @@ token = "insert-token-here"
 description  = "Lightning+, the successor to Lightning(.js)"
 
 # List Cogs that should be loaded on start
-cogs = ['cogs.comics', 'cogs.owner', 'cogs.load', 'cogs.moderation', 'cogs.logger', 'cogs.extras', 'cogs.mod_userlog', 'cogs.setup',
-'cogs.weeb', 'cogs.info', 'cogs.mod_note', 'cogs.fun', 'db.databasestart', 'cogs.toggle_roles', 'cogs.lockdown',
-'cogs.memes', 'cogs.poll', 'cogs.common', 
-'cogs.misc', 'cogs.status_switch', 'cogs.lightning-hub', 
-'cogs.timers']
+cogs = ['cogs.comics', 'cogs.owner', 'cogs.load', 'cogs.moderation', 'cogs.logger', 
+'cogs.extras', 'cogs.mod_userlog', 'cogs.setup',
+'cogs.weeb', 'cogs.info', 'cogs.mod_note', 'cogs.fun', 'db.databasestart', 'cogs.toggle_roles', 
+'cogs.lockdown', 'cogs.memes', 'cogs.poll', 'cogs.common', 'cogs.misc', 'cogs.status_switch', 
+'cogs.lightning-hub', 'cogs.timers', 'cogs.powerscrona']
 
 # Bot Errors/Logs Channel. 
 error_channel = 567109464184979502
+powerscron_errors = 575708612454907924
 
 #Sends errors to Webhook
 webhookurl = "INSERT_LINK_HERE"
 
 # Bot Owner ID
 owner_id = "INSERT_ID_HERE"
+
+# If you forked the repo and want to use the github commands
+github_username = "" # Github Username
+github_repo = "" # Repository Name
+github_key = "" # Personal Access Token (OAUTH Token)
+gh_whitelisted_guilds = [527887739178188830,
+                         540978015811928075] # Guilds that can use Github Commands
+
+# TheCatAPI Key (You can get one at thecatapi.com)
+catapi_token = ""
