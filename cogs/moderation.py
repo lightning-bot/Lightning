e6856069b6e7588b8bac9443b06a9a031f4f1144
LightSage <lightsage01@gmail.com>
2019-08-14 04:45:50 -0500

    Remove mod_note.py and merge commands into mod.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -50,7 +50,6 @@ class Moderation(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.bot.log.info(f'{self.qualified_name} loaded')
         self.db = dataset.connect('sqlite:///config/powerscron.sqlite3')
         
     async def cog_check(self, ctx):
@@ -676,6 +675,24 @@ class Moderation(commands.Cog):
         session.close()
         await ctx.send(f"{target.mention} can no longer speak. "
                        f"It will expire {duration_text}.")
+
+    @commands.guild_only()
+    @db.mod_check.check_if_at_least_has_staff_role("Helper")
+    @commands.command(aliases=["addnote"])
+    async def note(self, ctx, target: discord.Member, *, note: str = ""):
+        """Adds a note to a user, staff only."""
+        userlog(ctx.guild, target.id, ctx.author, note,
+                "notes", target.name)
+        await ctx.send(f"Noted {target}!")
+
+    @commands.guild_only()
+    @db.mod_check.check_if_at_least_has_staff_role("Helper")
+    @commands.command(aliases=["addnoteid"])
+    async def noteid(self, ctx, target: int, *, note: str = ""):
+        """Adds a note to a user by userid, staff only."""
+        userlog(ctx.guild, target, ctx.author, note,
+                "notes")
+        await ctx.send(f"ID {target} was noted!")
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
