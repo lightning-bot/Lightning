6688946bec67da0d17fc2176b89fc8e00033a916
LightSage <lightsage01@gmail.com>
2019-08-29 12:26:52 -0500

    Add command usage & tag tables (tags not ready)

--- schema.sql
+++ schema.sql
@@ -1,14 +1,14 @@
 -- Created for PostgreSQL 11
 
-DROP TABLE IF EXISTS cronjobs, staff_roles, userlogs, user_restrictions;
+DROP TABLE IF EXISTS cronjobs, staff_roles, userlogs, user_restrictions, tags, commands_usage;
 
 -- Just store the timestamps as utcnow(). 
 -- Makes my life easier
 CREATE TABLE cronjobs
 (
     id SERIAL PRIMARY KEY,
-    expiry timestamp without time zone,
-    created timestamp without time zone,
+    expiry timestamp without time zone DEFAULT (now() at time zone 'utc'), --DEFAULT (now() at time zone 'utc'),
+    created timestamp without time zone DEFAULT (now() at time zone 'utc'),
     event TEXT,
     extra JSONB
 );
@@ -54,3 +54,37 @@ CREATE TABLE auto_roles
     role_id BIGINT NOT NULL,
     CONSTRAINT auto_roles_pkey PRIMARY KEY (guild_id, role_id)
 );
+
+CREATE TABLE tags
+(
+    guild_id BIGINT PRIMARY KEY,
+    tag_name TEXT,
+    tag_content TEXT,
+    tag_author BIGINT,
+    created_at TIMESTAMP WITHOUT TIME ZONE,
+    usage BIGINT
+);
+
+CREATE TABLE tag_aliases
+(
+    guild_id BIGINT PRIMARY KEY,
+    tag_name TEXT,
+    tag_points_to TEXT REFERENCES tags(tag_name) ON DELETE CASCADE,
+    tag_author BIGINT,
+    created_at TIMESTAMP WITHOUT TIME ZONE
+);
+
+CREATE TABLE commands_usage
+(
+    id BIGSERIAL PRIMARY KEY,
+    guild_id BIGINT,
+    user_id BIGINT,
+    used_at TIMESTAMP WITHOUT TIME ZONE,-- DEFAULT (now() at time zone 'utc'),
+    command_name TEXT,
+    failure BOOLEAN
+);
+
+CREATE INDEX IF NOT EXISTS commands_usage_guild_id_idx ON commands_usage (guild_id);
+CREATE INDEX IF NOT EXISTS commands_usage_user_id_idx ON commands_usage (user_id);
+CREATE INDEX IF NOT EXISTS commands_usage_used_at_idx ON commands_usage (used_at);
+CREATE INDEX IF NOT EXISTS commands_usage_command_name_idx ON commands_usage (command_name);
