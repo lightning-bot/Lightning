ec2e1d4b72f346092f23c621d85fba56bd5d79af
LightSage <lightsage01@gmail.com>
2019-09-12 15:59:07 -0500

    Improve doc strings and flake8

--- cogs/emoji.py
+++ cogs/emoji.py
@@ -33,6 +33,7 @@ import random
 
 ROO_EMOTES = [604331487583535124, 604446987844190228, 606517600167526498, 610921560068456448]
 
+
 class Emoji(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -69,15 +70,17 @@ class Emoji(commands.Cog):
     @is_staff_or_has_perms("Helper", manage_emojis=True)
     async def add(self, ctx, emoji_name, *, url):
         """Adds the URL as an emoji to the guild
-        
-        Helpers+"""
+
+        In order to use this command, you must either have
+        Manage Emojis permission or a role that
+        is assigned as a Helper or above in the bot."""
         if len(emoji_name) > 32:
             return await ctx.send("Emoji name cannot be longer than 32 characters!")
         emoji_link = await self.bot.aiogetbytes(url)
         if emoji_link is not False:
             emoji_aio = self.aiobytesfinalize(emoji_link)
             try:
-                finalized_e = await ctx.guild.create_custom_emoji(name=emoji_name, image=emoji_aio, 
+                finalized_e = await ctx.guild.create_custom_emoji(name=emoji_name, image=emoji_aio,
                                                                   reason=f"Emoji Added by {ctx.author} "
                                                                   f"(ID: {ctx.author.id})")
             except Exception as ex:
@@ -95,8 +98,10 @@ class Emoji(commands.Cog):
     @is_staff_or_has_perms("Helper", manage_emojis=True)
     async def copy(self, ctx, emoji: discord.PartialEmoji):
         """ "Copies" an emoji and adds it to the guild.
-        
-        Helpers+"""
+
+        In order to use this command, you must either have
+        Manage Emojis permission or a role that
+        is assigned as a Helper or above in the bot."""
         emoji_link = await self.bot.aiogetbytes(str(emoji.url))
         if emoji_link is not False:
             try:
@@ -118,8 +123,10 @@ class Emoji(commands.Cog):
     @is_staff_or_has_perms("Helper", manage_emojis=True)
     async def delete(self, ctx, emote: discord.Emoji):
         """Deletes an emoji from the guild
-        
-        Moderators+"""
+
+        In order to use this command, you must either have
+        Manage Emojis permission or a role that
+        is assigned as a Moderator or above in the bot."""
         if ctx.guild.id != emote.guild_id:
             return await ctx.send("This emoji isn't in this guild!")
 
@@ -153,7 +160,7 @@ class Emoji(commands.Cog):
         """Prints a fancy list of the Roo Server's emotes"""
         if len(ctx.guild.emojis) == 0:
             return await ctx.send("This server has no emotes!")
-            
+
         paginator = commands.Paginator(suffix='', prefix='')
         for emoji in ctx.guild.emojis:
             paginator.add_line(f'{emoji} -- `{emoji}`')
@@ -213,6 +220,7 @@ class Emoji(commands.Cog):
                 msg = "✅ Emoji Added: "
                 msg += ", ".join(mk_emoji)
                 await emoji_chan.send(msg)
+
 
 def setup(bot):
     bot.add_cog(Emoji(bot))
