9ba4e143deff8c864c1c773d0f870d3a7d757eb1
LightSage <lightsage01@gmail.com>
2020-08-05 10:08:58 -0500

    Add fun cog
    New commands: screwedup, iq
    Removed commands: 8ball, cyrofreeze

---
+++ lightning/cogs/fun.py
@@ -0,0 +1,369 @@
+# Lightning.py - A multi-purpose Discord bot
+# Copyright (C) 2020 - LightSage
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation at version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import colorsys
+import io
+import math
+import random
+import textwrap
+import typing
+from datetime import datetime
+
+import discord
+import uwuify
+from discord.ext import commands
+from jishaku.functools import executor_function
+from PIL import Image, ImageDraw, ImageFont
+
+from lightning import LightningBot, LightningCog
+from lightning.utils import converters, flags, http
+from lightning.utils.errors import LightningError
+
+
+class Fun(LightningCog):
+    """Fun Stuff"""
+
+    def __init__(self, bot: LightningBot):
+        self.bot = bot
+
+    def c_to_f(self, c):
+        """stolen from Robocop-ng. """
+        return math.floor(9.0 / 5.0 * c + 32)
+
+    @executor_function
+    def make_kcdt(self, text: str) -> io.BytesIO:
+        img = Image.open("resources/templates/kurisudrawtemp.png")
+        dafont = ImageFont.truetype(font="resources/fonts/arialrounded.ttf",
+                                    size=42, encoding="unic")
+        draw = ImageDraw.Draw(img)
+        # Shoutouts to that person on stackoverflow that I don't remember
+        y_text = 228
+        wdmax = 560
+        lines = textwrap.wrap(text, width=20)
+        for line in lines:
+            if y_text >= 390:
+                break
+            line_width, line_height = draw.textsize(line, font=dafont)
+            draw.multiline_text(((wdmax - line_width) / 2, y_text),
+                                line, font=dafont,
+                                fill="black")  # align="center")
+            y_text += line_height
+        finalbuffer = io.BytesIO()
+        img.save(finalbuffer, 'png')
+        finalbuffer.seek(0)
+        return finalbuffer
+
+    @commands.command(aliases=['kurisudraw'])
+    @commands.cooldown(2, 60.0, commands.BucketType.guild)
+    @commands.has_permissions(attach_files=True)
+    async def kurisuwhiteboard(self, ctx, *, text: str):
+        """Kurisu can solve this, can you?"""
+        async with ctx.typing():
+            img_buff = await self.make_kcdt(text)
+            await ctx.send(file=discord.File(img_buff, filename="kurisudraw.png"))
+
+    @executor_function
+    def make_jpegify(self, _bytes) -> io.BytesIO:
+        img = Image.open(io.BytesIO(_bytes))
+
+        buff = io.BytesIO()
+        img.convert("RGB").save(buff, "jpeg", quality=random.randrange(1, 10))
+        buff.seek(0)
+
+        return buff
+
+    @commands.command(aliases=['needsmorejpeg'])
+    @commands.cooldown(3, 30.0, commands.BucketType.guild)
+    @commands.has_permissions(attach_files=True)
+    async def jpegify(self, ctx, image: str = converters.LastImage):
+        """Jpegify an image"""
+        async with ctx.typing():
+            image = http.Whitelisted_URL(image)
+            byte_data = await http.getbytes(self.bot.aiosession, image.url)
+            image_buffer = await self.make_jpegify(byte_data)
+            await ctx.send(file=discord.File(image_buffer, filename="jpegify.jpeg"))
+
+    @executor_function
+    def make_lakitu(self, text: str) -> io.BytesIO:
+        img = Image.open("resources/templates/lakitutemp.png")
+        verdana = ImageFont.truetype(font="resources/fonts/verdana.ttf",
+                                     size=86, encoding="unic")
+        draw = ImageDraw.Draw(img)
+        text = textwrap.wrap(text, width=19)
+        y_text = 200
+        wdmax = 1150
+        for line in text:
+            if y_text >= 706:
+                break
+            line_width, line_height = draw.textsize(line, font=verdana)
+            draw.multiline_text(((wdmax - line_width) / 2, y_text),
+                                line, font=verdana,
+                                fill="black")  # align="center")
+            y_text += line_height
+        finalbuffer = io.BytesIO()
+        img.save(finalbuffer, 'png')
+        finalbuffer.seek(0)
+        return finalbuffer
+
+    @commands.command()
+    @commands.cooldown(2, 60.0, commands.BucketType.guild)
+    @commands.has_permissions(attach_files=True)
+    async def lakitufyi(self, ctx, *, text: str):
+        """Makes a Lakitu FYI meme with your own text"""
+        async with ctx.typing():
+            image_buffer = await self.make_lakitu(text)
+            await ctx.send(file=discord.File(image_buffer, filename="fyi.png"))
+
+    async def get_user_avatar(self, user: typing.Union[discord.User, discord.Member]) -> bytes:
+        async with self.bot.aiosession.get(str(user.avatar_url_as(format="png"))) as resp:
+            avy_bytes = await resp.read()
+        return avy_bytes
+
+    @executor_function
+    def make_screwed_in_the_head(self, avatar_bytes) -> io.BytesIO:
+        base_image = Image.open("resources/templates/inthehead.png")
+        avatar = Image.open(io.BytesIO(avatar_bytes)).resize((64, 64)).convert("RGB")
+
+        mask = Image.new("L", avatar.size, 0)
+        mask_draw = ImageDraw.Draw(mask)
+        mask_draw.ellipse([(0, 0), avatar.size], fill=255)
+        mask = mask.resize((64, 64), Image.ANTIALIAS)
+
+        base_image.paste(avatar, (14, 43), mask=mask)
+
+        buffer = io.BytesIO()
+        base_image.save(buffer, "png")
+        buffer.seek(0)
+
+        return buffer
+
+    @commands.command()
+    @commands.cooldown(2, 60.0, commands.BucketType.guild)
+    async def screwedup(self, ctx, member: discord.Member = commands.default.Author):
+        """Miko Iino tells you that you are screwed up in the head"""
+        async with ctx.typing():
+            avy = await self.get_user_avatar(member)
+            image_buffer = await self.make_screwed_in_the_head(avy)
+            await ctx.send(file=discord.File(image_buffer, "screwedupinthehead.png"))
+
+    @executor_function
+    def make_fuji_iq(self, avatar_bytes) -> io.BytesIO:
+        base_image = Image.open("resources/templates/fujiwara-iq.png")
+        avatar = Image.open(io.BytesIO(avatar_bytes)).resize((165, 165)).convert("RGB")
+
+        mask = Image.new("L", avatar.size, 0)
+        mask_draw = ImageDraw.Draw(mask)
+        mask_draw.ellipse([(0, 0), avatar.size], fill=255)
+        mask = mask.resize((165, 165), Image.ANTIALIAS)
+
+        base_image.paste(avatar, (140, 26), mask=mask)
+
+        buffer = io.BytesIO()
+        base_image.save(buffer, "png")
+        buffer.seek(0)
+
+        return buffer
+
+    @commands.command()
+    @commands.cooldown(2, 60.0, commands.BucketType.guild)
+    async def iq(self, ctx, member: discord.Member = commands.default.Author):
+        """Your iq is 3"""
+        async with ctx.typing():
+            avy = await self.get_user_avatar(member)
+            image_buffer = await self.make_fuji_iq(avy)
+            await ctx.send(file=discord.File(image_buffer, "huh_my_iq_is.png"))
+
+    @commands.command(aliases=['roll'])
+    async def dice(self, ctx, number: int):
+        """Rolls a 1 to the specified number sided die"""
+        if number <= 0:
+            return await ctx.send("You can't roll that!")
+        number_ran = random.randint(1, number)
+        await ctx.send(f"🎲 You rolled a `{number}` sided die. | The die rolled on `{number_ran}`")
+
+    @commands.command()  # Technically more of a meme, but shrug ascii
+    async def bam(self, ctx, target: discord.Member):
+        """Bams a user"""
+        # :idontfeelsogood:
+        random_bams = ["n̟̤͙̠̤̖ǫ̺̻ͅw̴͍͎̱̟ ̷̭̖̫͙̱̪b͏͈͇̬̠̥ͅ&̻̬.̶̜͍̬͇̬ ҉̜̪̘̞👍̡̫͙͚͕ͅͅ", "n͢ow̢ ͜b&͢. ̷👍̷",
+                       "n҉̺o̧̖̱w̯̬̜̺̘̮̯ ͉͈͎̱̰͎͡b&̪̗̮̣̻͉.͍͖̪͕̤͔ ͢👍̵͙̯͍̫̬",
+                       "ńo̶̡͜w͘͟͏ ҉̶b̧&̧.̡͝ ̕👍̡͟", "n҉o̢͘͞w̢͢ ̢͏̢b͠&̴̛.̵̶ ̢́👍̴",
+                       "n̶̵̵̷̡̲̝̺o̵̶̷̴̜͚̥͓w̶̶̶̴͔̲͢͝ ḇ̶̷̶̵̡̨͜&̷̴̶̵̢̗̻͝.̷̵̴̶̮̫̰͆ 👍̵̶̵̶̡̡̹̹",
+                       "n̸̶̵̵̷̴̷̵̷̒̊̽ò̷̷̷̶̶̶̶̴̝ͥ̄w̶̶̷̶̵̴̷̶̴̤̑ͨ b̷̵̶̵̶̷̵̴̶̧͌̓&̵̶̵̶̷̴̵̴̻̺̓̑.̵̴̷̵̶̶̶̷̷̹̓̉ 👍",
+                       "no̥̊w ͜͠b̹̑&̛͕.̡̉ 👍̡̌",
+                       "n̐̆ow͘ ̌̑b͛͗&͗̂̍̒.̄ ͊👍͂̿͘",
+                       "ₙₒw b&. 👍", "n҉o҉w҉ b҉&. 👍"]
+
+        await ctx.send(f"{target} is {random.choice(random_bams)}")
+
+    @commands.command()  # Another meme
+    async def warm(self, ctx, user: discord.Member):
+        """Warms a user"""
+        celsius = random.randint(15, 100)
+        fahrenheit = self.c_to_f(celsius)
+        await ctx.send(f"{user} warmed. User is now {celsius}°C ({fahrenheit}°F).")
+
+    @commands.command(aliases=['cool', 'cold'])  # Another meme again
+    async def chill(self, ctx, user: discord.Member):
+        """Chills/cools a user"""
+        celsius = random.randint(-50, 15)
+        fahrenheit = self.c_to_f(celsius)
+        await ctx.send(f"{user} chilled. User is now {celsius}°C ({fahrenheit}°F).")
+
+    async def get_previous_messages(self, channel, limit: int = 10) -> discord.Message:
+        messages = await channel.history(limit=limit).flatten()
+        return random.choice(messages)
+
+    @commands.command(aliases=['owo', 'uwuify'])
+    @commands.cooldown(rate=1, per=5, type=commands.BucketType.channel)
+    async def owoify(self, ctx, *, text: commands.clean_content()):
+        """An owo-ifier.
+
+        Flag options (no arguments):
+        `--random`: Owoifies random text that was sent in the channel.
+        `--lastmessage` or `--lm`: Owoifies the last message sent in the channel."""
+        _flags = flags.boolean_flags(['--random', '--lastmessage'], text,
+                                     raise_errors=True,
+                                     flag_aliases={'--lm': '--lastmessage'})
+
+        if _flags['--random'] is True:
+            message = await self.get_previous_messages(ctx.channel, 12)
+            if message.content:
+                text = message.content
+            else:
+                raise LightningError('Failed to find any message content.')
+        elif _flags['--lastmessage'] is True:
+            messages = await ctx.channel.history(limit=2).flatten()
+            message = messages[1]
+            if message.content:
+                text = message.content
+            else:
+                raise LightningError('Failed to find message content in the previous message.')
+        else:
+            text = _flags['text']
+
+        uwutalk = uwuify.uwu(text, flags=(uwuify.SMILEY | uwuify.YU))
+        await ctx.send(uwutalk)
+
+    @commands.command()
+    async def lolice(self, ctx, *, user: discord.Member = commands.default.Author):
+        """Lolice chief"""
+        url = f'https://nekobot.xyz/api/imagegen?type=lolice&url={user.avatar_url_as(format="png")}'
+        data = await http.getjson(self.bot.aiosession, url)
+        embed = discord.Embed()
+        embed.set_image(url=data['message'])
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def awooify(self, ctx, *, user: discord.Member = commands.default.Author):
+        """Awooify a user"""
+        url = f'https://nekobot.xyz/api/imagegen?type=awooify&url={user.avatar_url_as(format="png")}'
+        data = await http.getjson(self.bot.aiosession, url)
+        embed = discord.Embed()
+        embed.set_image(url=data['message'])
+        await ctx.send(embed=embed)
+
+    @lolice.before_invoke
+    @owoify.before_invoke
+    @awooify.before_invoke
+    async def do_typing_before(self, ctx):
+        await ctx.trigger_typing()
+
+    @commands.command(aliases=['cade'])
+    async def cat(self, ctx):
+        """Gives you a random cat picture"""
+        capi = {"x-api-key": self.bot.config['tokens']['catapi']}
+        async with self.bot.aiosession.get(url='https://api.thecatapi.com/v1/images/search', headers=capi) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+            else:
+                # lol
+                return await ctx.send(f"https://http.cat/{resp.status} Try again later(?)")
+        embed = discord.Embed(title="Meow <:meowawauu:604760862049304608>",
+                              color=discord.Color(0x0c4189))
+        embed.set_image(url=data[0]['url'])
+        embed.set_footer(text="Powered by TheCatApi")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def dog(self, ctx):
+        """Random dog pics from dog.ceo"""
+        data = await http.getjson(self.bot.aiosession, "https://dog.ceo/api/breeds/image/random")
+        embed = discord.Embed(title="Bark 🐶", color=discord.Color.blurple())
+        embed.set_image(url=data['message'])
+        embed.set_footer(text="Powered by dog.ceo", icon_url="https://dog.ceo/img/favicon.png")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def xkcd(self, ctx, xkcd_number: int = None):
+        """Returns an embed with information about the specified xkcd comic.
+
+        If no value is supplied or the value isn't found, it gives the latest xkcd instead."""
+        xkcd_latest = await http.getjson(self.bot.aiosession, "https://xkcd.com/info.0.json")
+        xkcd_max = xkcd_latest.get("num")
+
+        if xkcd_number is not None and int(xkcd_number) > 0 and int(xkcd_number) < xkcd_max:
+            entry = int(xkcd_number)
+        else:
+            entry = xkcd_max
+
+        xkcd = await http.getjson(self.bot.aiosession, f"https://xkcd.com/{entry}/info.0.json")
+        if xkcd is False:
+            return await ctx.send("Something went wrong grabbing that XKCD!")
+
+        timestamp = datetime.strptime(f"{xkcd['year']}-{xkcd['month']}-{xkcd['day']}",
+                                      "%Y-%m-%d")
+        embed = discord.Embed(title=f"xkcd {xkcd['num']}: {xkcd['safe_title']}",
+                              url=f"https://xkcd.com/{xkcd['num']}",
+                              timestamp=timestamp, color=discord.Color(0x96A8C8))
+        embed.set_image(url=xkcd["img"])
+        embed.set_footer(text=xkcd["alt"])
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases=['pat'])
+    @commands.bot_has_permissions(embed_links=True)
+    async def headpat(self, ctx):
+        """Random headpat gifs"""
+        data = await http.getjson(self.bot.aiosession, "https://nekos.life/api/pat")
+        url = data["url"]
+        color_random = [int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1)]
+        embed = discord.Embed(title='Headpat, owo', colour=discord.Color.from_rgb(*color_random))
+        embed.set_image(url=url)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.bot_has_permissions(embed_links=True)
+    async def hug(self, ctx, *, member: converters.WeebActionConverter("hugged") = None):  # noqa: F821
+        """Hugs someone"""
+        url = "https://nekos.life/api/v2/img/hug"
+        data = await http.getjson(self.bot.aiosession, url)
+        color_random = [int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1)]
+        embed = discord.Embed(title=member, color=discord.Color.from_rgb(*color_random))
+        embed.set_image(url=data['url'])
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.bot_has_permissions(embed_links=True)
+    async def slap(self, ctx, *, person: converters.WeebActionConverter("slapped") = None):  # noqa: F821
+        """Slap yourself or someone."""
+        slap = await http.getjson(self.bot.aiosession, "https://nekos.life/api/v2/img/slap")
+        url = slap["url"]
+        color_random = [int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1)]
+        embed = discord.Embed(title=person, colour=discord.Color.from_rgb(*color_random))
+        embed.set_image(url=url)
+        await ctx.send(embed=embed)
+
+
+def setup(bot: LightningBot):
+    bot.add_cog(Fun(bot))
