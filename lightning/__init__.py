02d7d8eb09c1f4a5be723f9362f4f0498399d70d
LightSage <lightsage01@gmail.com>
2020-08-29 02:13:34 -0500

    Add meta information into __init__

--- lightning/__init__.py
+++ lightning/__init__.py
@@ -15,5 +15,6 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from .context import LightningContext  # noqa
-from .core import LightningBot, LightningCog  # noqa
+from lightning.context import LightningContext  # noqa
+from lightning.core import LightningBot, LightningCog  # noqa
+from lightning.meta import *  # noqa
