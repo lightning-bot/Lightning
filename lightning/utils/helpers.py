588eabf3f9f1183e1af28875b3707d85fc0649f8
LightSage <lightsage01@gmail.com>
2020-09-29 01:23:46 -0500

    Add haste

--- lightning/utils/helpers.py
+++ lightning/utils/helpers.py
@@ -278,3 +278,19 @@ async def request(url, session: aiohttp.ClientSession, *, timeout=180, method: s
                 return await resp.read()
         else:
             raise errors.HTTPException(resp)
+
+
+async def haste(session: aiohttp.ClientSession, text: str, instance: str = 'https://mystb.in/'):
+    """Posts to a haste instance and returns the link.
+
+    Parameters:
+    ------------
+    session: `aiohttp.ClientSession`
+
+    text: str
+        The text to post to the instance.
+
+    instance: str
+        Link to a haste instance. By default https://mystb.in/ is used."""
+    resp = await request(f"{instance}documents", session, method="POST", data=text)
+    return f"{instance}{resp['key']}"
