dd08ad9f931332943f48a3cca35100643499490f
LightSage <lightsage01@gmail.com>
2020-12-11 12:41:57 -0600

    Merge Eval and Owner into one Owner cog & update deps

--- lightning/cogs/owner.py
+++ lightning/cogs/owner.py
@@ -22,84 +22,13 @@ import traceback
 import asyncpg
 import discord
 import tabulate
-from discord.ext import commands
 from jishaku.codeblocks import Codeblock, codeblock_converter
-from jishaku.cog import JishakuBase, jsk
-from jishaku.exception_handling import ReplResponseReactor
-from jishaku.flags import SCOPE_PREFIX
-from jishaku.functools import AsyncSender
-from jishaku.metacog import GroupCogMeta
-from jishaku.paginators import PaginatorInterface, WrappedPaginator
-from jishaku.repl import AsyncCodeExecutor, get_var_dict_from_ctx
+from jishaku.cog import OPTIONAL_FEATURES, STANDARD_FEATURES
+from jishaku.features.baseclass import Feature
 
-from lightning import LightningBot, LightningCog, LightningContext, formatters
+from lightning import LightningBot, LightningContext, formatters
 from lightning.utils import helpers
 from lightning.utils import time as ltime
-
-
-class Eval(JishakuBase, metaclass=GroupCogMeta, command_parent=jsk):
-
-    @commands.command(name="py", aliases=['python', 'eval'])
-    async def jsk_py(self, ctx: LightningContext, *, argument: codeblock_converter) -> None:
-        """Direct evaluation of python code"""
-        arg_dict = get_var_dict_from_ctx(ctx, SCOPE_PREFIX)
-        arg_dict["_"] = self.last_result
-
-        scope = self.scope
-
-        try:
-            async with ReplResponseReactor(ctx.message):
-                with self.submit(ctx):
-                    executor = AsyncCodeExecutor(argument.content, scope, arg_dict=arg_dict)
-                    async for send, result in AsyncSender(executor):
-                        if result is None:
-                            continue
-
-                        self.last_result = result
-
-                        if isinstance(result, discord.File):
-                            send(await ctx.send(file=result))
-                        elif isinstance(result, discord.Embed):
-                            send(await ctx.send(embed=result))
-                        elif isinstance(result, PaginatorInterface):
-                            send(await result.send_to(ctx))
-                        else:
-                            if not isinstance(result, str):
-                                # repr all non-strings
-                                result = repr(result)
-
-                            if len(result) > 2000:
-                                # inconsistency here, results get wrapped in codeblocks when they are too large
-                                #  but don't if they're not. probably not that bad, but noting for later review
-                                paginator = WrappedPaginator(prefix='```py', suffix='```', max_size=1985)
-
-                                paginator.add_line(result)
-
-                                interface = PaginatorInterface(ctx.bot, paginator, owner=ctx.author)
-                                send(await interface.send_to(ctx))
-                            else:
-                                if result.strip() == '':
-                                    result = "\u200b"
-
-                                send(await ctx.send(formatters.codeblock(result.replace(self.bot.http.token,
-                                                                                        '[token omitted]'))))
-        finally:
-            scope.clear_intersection(arg_dict)
-
-    @commands.command()
-    async def leaveguild(self, ctx: LightningContext, guild_id: int) -> None:
-        """Leaves a guild that the bot is in via ID"""
-        server = self.bot.get_guild(guild_id)
-        if server is None:
-            await ctx.send('I\'m not in this server.')
-            return
-        await server.leave()
-        await ctx.send(f'Successfully left **{server.name}**')
-
-    @commands.command()
-    async def pip(self, ctx: LightningContext, *, argument: codeblock_converter):
-        """A shortcut for 'jsk sh pip3'."""
-        await ctx.invoke(self.jsk_shell, argument=Codeblock(argument.language, "pip3 " + argument.content))
 
 
 class CommandBug:
@@ -115,14 +44,22 @@ class CommandBug:
         return f"<CommandBug token={int(self)}>"
 
 
-class Owner(LightningCog):
-    def __init__(self, bot: LightningBot):
-        self.bot = bot
+class Owner(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
+    @Feature.Command(parent="jsk", name="leaveguild")
+    async def jsk_leaveguild(self, ctx: LightningContext, guild_id: int) -> None:
+        """Leaves a guild that the bot is in via ID"""
+        server = self.bot.get_guild(guild_id)
+        if server is None:
+            await ctx.send('I\'m not in this server.')
+            return
+        await server.leave()
+        await ctx.send(f'Successfully left **{server.name}**')
 
-    async def cog_check(self, ctx: LightningContext) -> bool:
-        return await self.bot.is_owner(ctx.author)
+    @Feature.Command(parent="jsk", name="pip")
+    async def jsk_pip(self, ctx: LightningContext, *, argument: codeblock_converter):
+        return await ctx.invoke(self.jsk_shell, argument=Codeblock(argument.language, "pip3 " + argument.content))
 
-    @commands.command()
+    @Feature.Command()
     async def fetchlog(self, ctx: LightningContext) -> None:
         """Sends the log file into the invoking author's DMs"""
         res = await helpers.dm_user(ctx.author, "Here's the current log file:", file=discord.File("lightning.log"))
@@ -131,14 +68,14 @@ class Owner(LightningCog):
         else:
             await ctx.message.add_reaction("✅")
 
-    @commands.group()
+    @Feature.Command()
     async def blacklist(self, ctx: LightningContext) -> None:
         """Blacklisting Management"""
         if ctx.invoked_subcommand is None:
             await ctx.send_help(ctx.command)
             return
 
-    @blacklist.command(name="adduser", aliases=["blacklist-user"])
+    @Feature.Command(parent="blacklist", name="adduser")
     async def blacklist_user(self, ctx: LightningContext, user_id: int, *, reason: str = "No Reason Provided") -> None:
         """Blacklist an user from using the bot"""
         blacklist = self.bot.blacklisted_users
@@ -151,7 +88,7 @@ class Owner(LightningCog):
         await blacklist.add(user_id, reason)
         await ctx.send(f"✅ Successfully blacklisted user `{user_id}`")
 
-    @blacklist.command(name="removeuser")
+    @Feature.Command(parent="blacklist", name="removeuser")
     async def unblacklist_user(self, ctx: LightningContext, user_id: int) -> None:
         """Unblacklist an user from using the bot"""
         blacklist = self.bot.blacklisted_users
@@ -161,7 +98,7 @@ class Owner(LightningCog):
         await blacklist.pop(user_id)
         await ctx.send(f"✅ Successfully unblacklisted user `{user_id}`")
 
-    @blacklist.command(name="search")
+    @Feature.Command(parent="blacklist", name="search")
     async def search_blacklist(self, ctx: LightningContext, user_id: int) -> None:
         """Search the blacklist to see if a user is blacklisted"""
         if str(user_id) in self.bot.blacklisted_users:
@@ -170,16 +107,16 @@ class Owner(LightningCog):
         else:
             await ctx.send("No matches found!")
 
-    @commands.command()
+    @Feature.Command()
     async def approve(self, ctx: LightningContext, guild_id: int) -> None:
         """Approves a server.
 
         Server must have already existed in the database before."""
-        query = "UPDATE guilds SET whitelisted='t' WHERE id=$1"
+        query = "UPDATE guilds SET whitelisted='t' WHERE id=$1;"
         await self.bot.pool.execute(query, guild_id)
         await ctx.tick(True)
 
-    @commands.command()
+    @Feature.Command()
     async def unapprove(self, ctx: LightningContext, guild_id: int) -> None:
         """Unapproves a server"""
         query = "UPDATE guilds SET whitelisted='f' WHERE id=$1"
@@ -191,7 +128,7 @@ class Owner(LightningCog):
 
         await ctx.tick(True)
 
-    @commands.command(aliases=['status'])
+    @Feature.Command(aliases=['status'])
     async def playing(self, ctx: LightningContext, *, gamename: str = None) -> None:
         """Sets the bot's playing message."""
         if not gamename:
@@ -202,14 +139,14 @@ class Owner(LightningCog):
         await self.bot.change_presence(activity=discord.Game(name=gamename))
         await ctx.send(f'Successfully changed status to `{gamename}`')
 
-    @commands.command()
+    @Feature.Command()
     async def exit(self, ctx: LightningContext) -> None:
         """Stops the bot"""
         shutdown_messages = ['Shutting Down...', "See ya!", "RIP", "Turning off...."]
         await ctx.send(f"{helpers.Emoji.greentick} {random.choice(shutdown_messages)}")
         await self.bot.logout()
 
-    @commands.command()
+    @Feature.Command()
     async def sql(self, ctx: LightningContext, *, query: codeblock_converter) -> None:
         """Run some SQL"""
         if query.count(";") > 1:
@@ -237,12 +174,12 @@ class Owner(LightningCog):
             to_send = f"Took {round(dt)}ms\n{formatters.codeblock(output, language='')}"
         await ctx.send(to_send)
 
-    @commands.group(invoke_without_command=True)
+    @Feature.Command(invoke_without_command=True)
     async def bug(self, ctx: LightningContext) -> None:
         """Commands to manage the bug system"""
         await ctx.send_help("bug")
 
-    @bug.command(name='view', aliases=['show'])
+    @Feature.Command(parent="bug", name='view', aliases=['show'])
     async def viewbug(self, ctx: LightningContext, token: str) -> None:
         """Views a bug's traceback"""
         query = """SELECT traceback, created_at, token FROM command_bugs
@@ -258,7 +195,7 @@ class Owner(LightningCog):
                               description=formatters.codeblock(bug.traceback))
         await ctx.send(embed=embed)
 
-    @bug.command(name='delete', aliases=['remove', 'rm'])
+    @Feature.Command(parent="bug", name='delete', aliases=['remove', 'rm'])
     async def deletebug(self, ctx: LightningContext, token: str) -> None:
         """Deletes a bug if it exists"""
         query = "DELETE FROM command_bugs WHERE token=$1;"
@@ -269,7 +206,7 @@ class Owner(LightningCog):
 
         await ctx.send(f"Deleted bug {token}")
 
-    @bug.command(name='list', aliases=['recent'])
+    @Feature.Command(parent="bug", name='list', aliases=['recent'])
     async def listbugs(self, ctx: LightningContext, limit: int = 10) -> None:
         """Lists the most recent bugs"""
         query = """SELECT * FROM command_bugs
@@ -288,7 +225,29 @@ class Owner(LightningCog):
         embed.set_footer(text=f"{total} bugs")
         await ctx.send(embed=embed)
 
+    @Feature.Command()
+    async def prettycommandlist(self, ctx: LightningContext) -> None:
+        commands = []
+        cogs = sorted(self.bot.cogs.values(), key=lambda c: c.qualified_name)
+        for cog in cogs:
+            cmds = []
+            for command in cog.walk_commands():
+                aliases = ' '.join("`{}`".format(r) for r in command.aliases) or "None"
+                signature = command.signature or command.usage
+                usage = f".{command.qualified_name}"
+                if signature:
+                    usage += f" {signature}"
+
+                description = command.help.replace('\n', '<br>') if command.help else None
+
+                cmds.append((command.qualified_name, aliases, description, f"`{usage}`"))
+            commands.append((cog.qualified_name, cog.description, tabulate.tabulate(cmds,
+                             headers=("Name", "Aliases", "Description", "Usage"), tablefmt="github")))
+
+        e = ["# {}\n{}\n\n{}\n\n".format(c, d, t) for c, d, t in commands]
+        content = "".join(e)
+        await ctx.send(content)
+
 
 def setup(bot: LightningBot) -> None:
-    bot.add_cog(Owner(bot))
-    bot.add_cog(Eval(bot))
+    bot.add_cog(Owner(bot=bot))
