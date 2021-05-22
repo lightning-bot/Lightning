cd3b5b18613de64e46b121d943436a3877cf1511
LightSage <lightsage01@gmail.com>
2021-05-22 16:40:56 +0000

    Require 3.8+, update deps, bump version to 3.2.0-alpha

--- lightning/meta.py
+++ lightning/meta.py
@@ -21,6 +21,6 @@ __all__ = ('__title__',
            '__author__')
 
 __title__ = "Lightning"
-__version__ = "3.1.0"
+__version__ = "3.2.0-alpha"
 __license__ = "GNU Affero General Public License Version 3"
 __author__ = "LightSage"
