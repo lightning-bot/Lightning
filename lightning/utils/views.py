575cdbab54e8ff110b71a33b1ef3680009bcc78a
LightSage <lightsage01@gmail.com>
2021-07-10 14:16:08 +0000

    utils: Add view utils

---
+++ lightning/utils/views.py
@@ -0,0 +1,92 @@
+"""
+Lightning.py - A Discord bot
+Copyright (C) 2019-2021 LightSage
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation at version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import asyncio
+
+import discord
+
+
+class BaseView(discord.ui.View):
+    """A view that adds cleanup to it"""
+    async def cleanup(self):
+        """Coroutine that cleans up something (like database connections, whatever).
+
+        This is called either when the view has timed out or when the view has stopped."""
+        pass
+
+    async def on_timeout(self):
+        await self.cleanup()
+
+    def stop(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.cleanup())
+        super().stop()
+
+
+class MenuLikeView(BaseView):
+    """A view that mimics similar behavior of discord.ext.menus.
+
+    This is intended for use with discord.ui.buttons, but you can use discord.ui.select too.
+
+    Parameters
+    ----------
+    clear_view_after : bool
+        Whether to remove the view after the view is done or timed out. Defaults to False
+    delete_message_after : bool
+        Whether to delete the message after the view is done or timed out. Defaults to False
+    timeout : Optional[float]
+        Defines when the view should stop listening for the interaction event."""
+    def __init__(self, *, clear_view_after=False, delete_message_after=False, timeout=180.0):
+        super().__init__(timeout=timeout)
+        self.ctx = None
+        self.clear_view_after = clear_view_after
+        self.delete_message_after = delete_message_after
+
+    # Not sure about this yet, might change it
+    def initial_message(self, ctx):
+        raise NotImplementedError
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        return (self.ctx.author.id == interaction.user.id and  # noqa
+                self.ctx.channel.id == interaction.channnel.id)  # noqa
+
+    def _assume_message_kwargs(self, value):  # "Assumes" kwargs that are passed to message.send
+        if isinstance(value, dict):
+            return value
+        elif isinstance(value, str):
+            return {'content': value, 'embed': None}
+        elif isinstance(value, discord.Embed):
+            return {'embed': value, 'content': None}
+
+    async def start(self, ctx, *, channel=None, wait=True):
+        self.ctx = ctx
+
+        dest = channel or ctx.channel
+
+        kwargs = self._assume_message_kwargs(self.initial_message(ctx))
+        self.message = await dest.send(**kwargs, view=self)
+
+        if wait:
+            await self.wait()
+
+    async def cleanup(self):
+        # This is first for obvious reasons
+        if self.delete_message_after:
+            await self.message.delete()
+            return
+
+        if self.clear_view_after:
+            await self.message.edit(view=None)
