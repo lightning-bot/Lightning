b67201ab7629927e0c4b24338e55b0b89b565b92
LightSage <lightsage01@gmail.com>
2021-07-13 16:28:47 +0000

    utils: Add a confirmation view

---
+++ lightning/utils/views.py
@@ -0,0 +1,67 @@
+"""
+Lightning.py - A Discord bot
+Copyright (C) 2019-2021 LightSage
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation at version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import discord
+
+from lightning.utils.helpers import Emoji
+from lightning.views import MenuLikeView
+
+
+class ConfirmationView(MenuLikeView):
+    """A confirmation view.
+
+    This is similar to ConfirmationMenu, except it uses discord bot kit ui instead.
+
+    Parameters
+    ----------
+    message : str
+        The message to send
+    timeout : float
+        When the view should time out
+    include_help_message : bool
+        Whether to include a help message in the message sent
+    **kwargs
+        Kwargs that are passed to MenuLikeView
+
+    Returns
+    -------
+    Optional[bool]
+        ``True`` if explicit confirm,
+        ``False`` if explicit deny,
+        ``None`` if deny due to timeout
+    """
+
+    def __init__(self, message: str, *, timeout=60.0, include_help_message: bool = False, **kwargs):
+        super().__init__(timeout=timeout, **kwargs)
+        self.msg = message
+        self.include_help_message = include_help_message
+        self.value = None
+
+    def format_initial_message(self, ctx) -> str:
+        if self.include_help_message:
+            return self.msg + "\n\nPress the buttons below to respond."
+        else:
+            return self.msg
+
+    @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green, emoji=Emoji.greentick)
+    async def confirm(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.value = True
+        self.stop()
+
+    @discord.ui.button(label='Deny', style=discord.ButtonStyle.red, emoji=Emoji.redtick)
+    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.value = False
+        self.stop()
