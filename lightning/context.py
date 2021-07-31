2931c0b160d07039b632c977f498cb641345b20e
LightSage <lightsage01@gmail.com>
2021-07-31 21:56:05 +0000

    Set delete_after to True by default

--- lightning/context.py
+++ lightning/context.py
@@ -51,7 +51,7 @@ class LightningContext(commands.Context):
             except discord.Forbidden:
                 await self.message.add_reaction(emoji)
 
-    async def confirm(self, message: str, *, delete_after=False, include_help_message=False) -> bool:
+    async def confirm(self, message: str, *, delete_after=True, include_help_message=False) -> bool:
         view = ConfirmationView(message, delete_message_after=delete_after,
                                 include_help_message=include_help_message)
         await view.start(self)
