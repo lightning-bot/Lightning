7ac49c083245a75dbbb800b8cb14ff9aec2a5622
LightSage <lightsage01@gmail.com>
2022-05-12 08:18:11 -0500

    Move Emoji over to constants, Remove ConfirmationMenu

--- lightning/constants.py
+++ lightning/constants.py
@@ -16,3 +16,28 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 COMMON_HOIST_CHARACTERS = ["!", "-", "/", "*", "(", ")", "+", "[", "]", "#", "<", ">", "_", ".", "$", "\"", "?"]
+
+
+class Emoji:
+    greentick = "<:greenTick:613702930444451880>"
+    redtick = "<:redTick:613703043283681290>"
+    member_leave = "<:member_leave:613363354357989376>"
+    member_join = "<:member_join:613361413272109076>"
+    python = "<:python:605592693267103744>"
+    dpy = "<:dpy:617883851162779648>"
+    postgres = "<:postgres:617886426318635015>"
+    # Presence emojis
+    do_not_disturb = "<:dnd:572962188134842389>"
+    online = "<:online:572962188114001921>"
+    idle = "<:idle:572962188201820200>"
+    offline = "<:offline:572962188008882178>"
+    numbers = ('1\N{combining enclosing keycap}',
+               '2\N{combining enclosing keycap}',
+               '3\N{combining enclosing keycap}',
+               '4\N{combining enclosing keycap}',
+               '5\N{combining enclosing keycap}',
+               '6\N{combining enclosing keycap}',
+               '7\N{combining enclosing keycap}',
+               '8\N{combining enclosing keycap}',
+               '9\N{combining enclosing keycap}',
+               '\N{KEYCAP TEN}')
