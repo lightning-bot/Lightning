26f973ab1493765f49e7d63202a99d4a937c52b3
LightSage <lightsage01@gmail.com>
2022-06-14 09:52:24 -0500

    Refactor flags

--- lightning/flags.py
+++ lightning/flags.py
@@ -18,6 +18,8 @@ import inspect
 from types import SimpleNamespace
 from typing import Any, Dict, List, Optional
 
+import discord
+from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands.converter import (CONVERTER_MAPPING, Converter,
                                             _convert_to_bool)
@@ -32,6 +34,7 @@ __all__ = ("Flag",
            "add_flag",
            "FlagParser",
            "FlagCommand",
+           "HybridFlagCommand",
            "FlagGroup")
 
 
@@ -68,7 +71,7 @@ class Flag:
         A converter to convert the argument passed for the flag.
 
         By default, converts to str
-    attr_name : Optional[str], optional
+    attribute : Optional[str], optional
         Attribute name in the namespace. If no attribute name is given, defaults to the first flag name.
     default : Optional[Any], optional
         A default argument for a flag, by default None
@@ -76,6 +79,8 @@ class Flag:
         Whether the flag requires an argument, by default False
     is_bool_flag : bool, optional
         Whether the flag should be marked as a boolean flag, by default False
+    consume_rest : bool, optional
+        Whether the flag is a consume rest flag, by default False
 
     Raises
     ------
@@ -85,19 +90,21 @@ class Flag:
         Raised when a bool flag has been marked as required
     """
 
-    __slots__ = ('names', 'help', 'converter', 'attr_name', 'default', 'required', 'is_bool_flag')
+    __slots__ = ('names', 'help', 'converter', 'attribute', 'default', 'required', 'is_bool_flag', 'consume_rest')
 
-    def __init__(self, *names, help: Optional[str] = None, converter: Any = str, attr_name: Optional[str] = None,
-                 default: Optional[Any] = None, required: bool = False, is_bool_flag: bool = False):
+    def __init__(self, *names, help: Optional[str] = None, converter: Any = str, attribute: Optional[str] = None,
+                 default: Optional[Any] = None, required: bool = False, is_bool_flag: bool = False,
+                 consume_rest: bool = False):
         for name in names:
             if name[0] != "-":
                 raise TypeError("A flag name must start with \"-\"")
+
         self.names = names
         self.help = help
         self.converter = converter
 
-        attr_name = attr_name if attr_name is not None else names[0]
-        self.attr_name = attr_name.strip("-").replace("-", "_")
+        attribute = attribute if attribute is not None else names[0]
+        self.attribute = attribute.strip("-").replace("-", "_")
 
         self.default = default
         self.required = required
@@ -107,18 +114,20 @@ class Flag:
             self.default = bool(default)
 
         if self.required and is_bool_flag:
-            raise FlagError("Boolean flags cannot require an argument")
+            raise TypeError("Boolean flags cannot require an argument")
 
         self.is_bool_flag = is_bool_flag
+
+        # Rest flag type
+        if consume_rest and is_bool_flag:
+            raise TypeError("consume_rest and is_bool_flag cannot be mixed")
+
+        self.consume_rest = consume_rest
 
 
 def add_flag(*names, **kwargs):
     def deco(func):
-        if isinstance(func, commands.Command):
-            funct = func.callback
-        else:
-            funct = func
-
+        funct = func.callback if isinstance(func, commands.Command) else func
         if not hasattr(funct, '__lightning_argparser__'):
             funct.__lightning_argparser__ = FlagParser()
 
@@ -133,15 +142,12 @@ class Namespace(SimpleNamespace):
 
 
 class FlagParser:
-    def __init__(self, flag_options: List[Flag] = [], *, raise_on_bad_flag: bool = True, consume_rest: bool = True,
-                 rest_converter=None, rest_attribute_name: str = "rest"):
+    def __init__(self, flag_options: List[Flag] = None, *, raise_on_bad_flag: bool = True):
         self.raise_on_bad_flag = raise_on_bad_flag
-        self.rest_attribute_name = rest_attribute_name
-        self.rest_converter = rest_converter
-        self.consume_rest = consume_rest
+        self.consume_rest_flag: Flag = None
         self._flags: Dict[str, Flag] = {}
 
-        self._register_flags(flag_options)
+        self._register_flags(flag_options or [])
 
     def add_flag(self, flag: Flag) -> None:
         """Adds a flag to the parser
@@ -151,11 +157,13 @@ class FlagParser:
         flag : Flag
             A :class:`Flag` instance
         """
-        for idx, name in enumerate(flag.names):
-            if idx == 0:
-                if self.rest_attribute_name == name:
-                    raise FlagError(f"{self.rest_attribute_name} is reserved.")
+        if flag.consume_rest:
+            if self.consume_rest_flag is not None:
+                raise FlagError("You can only define one consume_rest flag")
+            self.consume_rest_flag = flag
+            return
 
+        for name in flag.names:
             if name in self._flags:
                 raise FlagError(f"Flag name \"{name}\" is already registered.")
             else:
@@ -224,17 +232,14 @@ class FlagParser:
 
             raise commands.BadArgument(f'Converting to "{name}" failed for flag "{passed_flag}".') from exc
 
-    def _prepare_namespace(self) -> dict:
-        ns = {}
+    def _prepare_raw_namespace(self) -> Dict[str, Any]:
         flags = self.get_all_unique_flags()
-        for flag in flags:
-            ns[flag.attr_name] = flag.default  # More and likely it's probably None...
-        return ns
+        return {flag.attribute: flag.default for flag in flags}
 
-    async def parse_args(self, ctx):
-        view = FlagView(ctx.view.read_rest())
-        view.skip_ws()
-        ns = self._prepare_namespace()
+    async def parse_args(self, ctx, argument: Optional[str] = None) -> Namespace:
+        argument = argument or ctx.view.read_rest()
+        view = FlagView(argument.lstrip())
+        ns = self._prepare_raw_namespace()
         rest = []
         while not view.eof:
             word = view.get_word()
@@ -257,22 +262,22 @@ class FlagParser:
                     continue
 
                 if flag.is_bool_flag is True:
-                    ns[flag.attr_name] = True
+                    ns[flag.attribute] = True
                     continue
 
                 view.skip_ws()
                 next_arg = view.get_quoted_word()
-                ns[flag.attr_name] = await self.convert_flag_type(flag, ctx, next_arg, stripped)
+                ns[flag.attribute] = await self.convert_flag_type(flag, ctx, next_arg, stripped)
             else:
                 rest.append(word)
                 continue
 
         for flag in self.get_all_unique_flags():
-            if ns[flag.attr_name] is None and flag.required is True:
+            if ns[flag.attribute] is None and flag.required is True:
                 raise MissingRequiredFlagArgument(flag.names[0])
 
-        if self.consume_rest:
-            ns[self.rest_attribute_name] = ''.join(rest) or None
+        if self.consume_rest_flag:
+            ns[self.consume_rest_flag.attribute] = ''.join(rest) or None
 
         return Namespace(**ns)
 
@@ -293,7 +298,7 @@ class FlagCommand(LightningCommand):
 
         raise_bad_flag = kwargs.pop('raise_bad_flag', parser.raise_on_bad_flag)
         rest_usage_name = kwargs.pop('rest_attribute_name', None)
-        flag_consume_rest = kwargs.pop('flag_consume_rest', parser.consume_rest)
+        flag_consume_rest = kwargs.pop('flag_consume_rest', parser.consume_rest_flag)
 
         if flag_consume_rest is False and rest_usage_name is not None:
             raise TypeError("Cannot specify rest_attribute_name if not consuming rest")
@@ -325,30 +330,30 @@ class FlagCommand(LightningCommand):
             if flag.required:
                 # Required flags shouldn't have defaults?
                 sig.append(f"<{flag.names[0]}>")
+            elif flag.default:
+                sig.append(f"[{flag.names[0]}={flag.default}]")
             else:
-                if flag.default:
-                    sig.append(f"[{flag.names[0]}={flag.default}]")
-                else:
-                    sig.append(f"[{flag.names[0]}]")
+                sig.append(f"[{flag.names[0]}]")
 
         return ' '.join(sig)
 
     async def _parse_flag_args(self, ctx: commands.Context):
         args = await self.callback.__lightning_argparser__.parse_args(ctx)
-        ctx.kwargs.update(vars(args))
+        return args
 
     async def _parse_arguments(self, ctx: commands.Context):
         ctx.args = [ctx] if self.cog is None else [self.cog, ctx]
         ctx.kwargs = {}
         args = ctx.args
         kwargs = ctx.kwargs
+        attachments = commands.core._AttachmentIterator(ctx.message.attachments)
 
         view = ctx.view
         iterator = iter(self.params.items())
 
         for name, param in iterator:
             if param.kind == param.POSITIONAL_OR_KEYWORD:
-                transformed = await self.transform(ctx, param)
+                transformed = await self.transform(ctx, param, attachments)
                 args.append(transformed)
             elif param.kind == param.KEYWORD_ONLY:
                 # kwarg only param denotes "consume rest" semantics
@@ -359,7 +364,7 @@ class FlagCommand(LightningCommand):
                     raise commands.MissingRequiredArgument(param)
                 while not view.eof:
                     try:
-                        transformed = await self.transform(ctx, param)
+                        transformed = await self.transform(ctx, param, attachments)
                         args.append(transformed)
                     except RuntimeError:
                         break
@@ -367,9 +372,103 @@ class FlagCommand(LightningCommand):
                 await self._parse_flag_args(ctx)
                 break
 
-        if not self.ignore_extra:
-            if not view.eof:
-                raise commands.TooManyArguments('Too many arguments passed to ' + self.qualified_name)
+        if not self.ignore_extra and not view.eof:
+            raise commands.TooManyArguments(f'Too many arguments passed to {self.qualified_name}')
+
+
+def replace_parameters(
+    parameters: Dict[str, commands.Parameter], callback, signature: inspect.Signature
+) -> List[inspect.Parameter]:
+    # Need to convert commands.Parameter back to inspect.Parameter so this will be a bit ugly
+    params = signature.parameters.copy()
+    for name, parameter in parameters.items():
+        converter = parameter.converter
+
+        # Parameter.converter properly infers from the default and has a str default
+        # This allows the actual signature to inherit this property
+        param = params[name].replace(annotation=converter)
+        param = commands.hybrid.replace_parameter(param, converter, callback, parameter, params)
+
+        if parameter.default is not parameter.empty:
+            default = commands.hybrid._CallableDefault(parameter.default) if callable(
+                parameter.default) else parameter.default
+            param = param.replace(default=default)
+
+        if isinstance(param.default, inspect.Parameter):
+            # If we're here, then then it hasn't been handled yet so it should be removed completely
+            param = param.replace(default=parameter.empty)
+
+        if hasattr(callback, '__lightning_argparser__') and name == 'flags' and converter == str:
+            parser: FlagParser = callback.__lightning_argparser__
+            for flag in parser.get_all_unique_flags():
+                transformed = inspect.Parameter(flag.attribute, parameter.kind, default=flag.default,
+                                                annotation=bool if flag.is_bool_flag else flag.converter)
+                param = commands.hybrid.replace_parameter(
+                    transformed, transformed.annotation, callback, parameter, params)
+
+                params[flag.attribute] = param
+
+            if parser.consume_rest_flag:
+                params[parser.consume_rest_flag.attribute] = param.replace(
+                    name=parser.consume_rest_flag.attribute, default=None, annotation=str)
+
+            del params[name]
+            continue
+
+        # Flags are flattened out and thus don't get their parameter in the actual mapping
+        if hasattr(converter, '__commands_is_flag__'):
+            del params[name]
+            continue
+
+        params[name] = param
+
+    return list(params.values())
+
+
+commands.hybrid.replace_parameters = replace_parameters
+
+
+class HybridAppCommand(commands.hybrid.HybridAppCommand):
+    async def _transform_arguments(self, interaction: discord.Interaction,
+                                   namespace: app_commands.Namespace) -> Dict[str, Any]:
+        transformed_values = await super()._transform_arguments(interaction, namespace)
+
+        if hasattr(self.callback, "__lightning_argparser__"):
+            parser: FlagParser = self.callback.__lightning_argparser__
+            raw_ns = parser._prepare_raw_namespace()
+            for f in parser.get_all_unique_flags():
+                f: Flag
+                try:
+                    value = transformed_values.pop(f.attribute)
+                except KeyError:
+                    raise app_commands.CommandSignatureMismatch(self) from None
+                else:
+                    raw_ns[f.attribute] = value
+
+            if parser.consume_rest_flag:
+                try:
+                    value = transformed_values.pop(parser.consume_rest_flag.attribute)
+                except KeyError:
+                    raise app_commands.CommandSignatureMismatch(self) from None
+                else:
+                    raw_ns[parser.consume_rest_flag.attribute] = value
+
+            transformed_values["flags"] = Namespace(**raw_ns)
+
+        return transformed_values
+
+
+class HybridFlagCommand(commands.HybridCommand, FlagCommand):
+    """
+    A hybrid flag command.
+
+    Your flags parameter must be named "flags" for the arguments to be converted.
+    """
+    def __init__(self, func, /, **kwargs: Any) -> None:
+        super().__init__(func, **kwargs)
+
+        if self.app_command:
+            self.app_command = HybridAppCommand(self)
 
 
 class FlagGroup(LightningGroupCommand, FlagCommand):
