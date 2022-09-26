09bcd69ee1c85a48681d9d5800c4a4d00cef4121
LightSage <lightsage01@gmail.com>
2022-09-26 13:37:28 -0500

    Drop all yoyo related tables
    How many tables does it need for real

--- migrations/0003_logging.sql
+++ migrations/0003_logging.sql
@@ -5,3 +5,8 @@ CREATE TABLE IF NOT EXISTS pastes
     code TEXT,
     delete_token TEXT
 );
+
+DROP TABLE IF EXISTS _yoyo_log;
+DROP TABLE IF EXISTS yoyo_lock;
+DROP TABLE IF EXISTS _yoyo_migration;
+DROP TABLE IF EXISTS _yoyo_version;
