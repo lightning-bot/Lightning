92758f773bc2433539be77aadcf38ffe12d37625
LightSage <lightsage01@gmail.com>
2022-10-25 09:21:42 -0500

    Move AutoMod to its own plugin

--- lightning/cogs/config/cog.py
+++ lightning/cogs/config/cog.py
@@ -18,26 +18,18 @@ from __future__ import annotations
 
 import collections
 import logging
-from typing import Any, Dict, List, Literal, Optional, Tuple, Union
+from typing import Literal, Optional, Tuple, Union
 
 import discord
 from discord.ext import commands
-from sanctum import DataConflict, NotFound
 
 from lightning import CommandLevel, GuildContext, LightningCog, cache, group
 from lightning.cogs.config import ui
-from lightning.cogs.config.converters import (AutoModDuration,
-                                              AutoModDurationResponse,
-                                              IgnorableEntities)
-from lightning.constants import (AUTOMOD_EVENT_NAMES_LITERAL,
-                                 AUTOMOD_EVENT_NAMES_MAPPING)
 from lightning.converters import Role, ValidCommandName, convert_to_level_value
 from lightning.formatters import plural
 from lightning.models import GuildModConfig
 from lightning.utils.checks import has_guild_permissions
 from lightning.utils.helpers import ticker
-from lightning.utils.paginator import Paginator
-from lightning.utils.time import ShortTime
 
 log = logging.getLogger(__name__)
 
@@ -67,40 +59,11 @@ class Configuration(LightningCog):
         query = f"UPDATE {table} SET {key} = NULL WHERE guild_id=$1;"
         return await self.bot.pool.execute(query, guild_id)
 
-    @group(invoke_without_command=True, level=CommandLevel.Admin)
+    @group(invoke_without_command=True)
     @has_guild_permissions(manage_guild=True)
     async def config(self, ctx: GuildContext) -> None:
         """Manages most of the configuration for the bot"""
         await ctx.send_help('config')
-
-    async def add_prefix(self, guild: discord.Guild, prefix: list, *, connection=None) -> None:
-        """Adds a prefix to the guild's config"""
-        query = """INSERT INTO guild_config (guild_id, prefixes)
-                   VALUES ($1, $2::text[]) ON CONFLICT (guild_id)
-                   DO UPDATE SET
-                        prefix = EXCLUDED.prefix;
-                """
-        connection = connection or self.bot.pool
-        await connection.execute(query, guild.id, list(prefix))
-
-    async def get_guild_prefixes(self, guild_id: int, connection=None) -> list:
-        connection = connection or self.bot.pool
-        query = """SELECT prefixes
-                   FROM guild_config
-                   WHERE guild_id=$1;"""
-        val = await connection.fetchval(query, guild_id)
-        return val or []
-
-    async def delete_prefix(self, guild_id: int, prefixes: list) -> str:
-        """Deletes a prefix"""
-        if not prefixes:
-            query = "UPDATE guild_config SET prefixes = NULL WHERE guild_id = $1;"
-            args = [guild_id]
-        else:
-            query = "UPDATE guild_config SET prefixes = $1 WHERE guild_id = $2;"
-            args = [prefixes, guild_id]
-
-        return await self.bot.pool.execute(query, *args)
 
     @config.command(level=CommandLevel.Admin)
     @has_guild_permissions(manage_guild=True)
@@ -235,149 +198,6 @@ class Configuration(LightningCog):
         query = "UPDATE roles SET punishment_roles = array_remove(punishment_roles, $1) WHERE guild_id=$2;"
         await self.bot.pool.execute(query, config.mute_role_id, ctx.guild.id)
         await ctx.send(f"Unbound {plural(users):user} from the mute role.")
-
-    # AutoMod
-
-    @config.group(level=CommandLevel.Admin)
-    @has_guild_permissions(manage_guild=True)
-    async def automod(self, ctx: GuildContext) -> None:
-        """Commands to configure Lightning's Auto-Moderation"""
-        if ctx.invoked_subcommand is None:
-            await ctx.send_help(ctx.command)
-
-    @automod.command(level=CommandLevel.Admin, name='ignore', require_var_positional=True)
-    @has_guild_permissions(manage_guild=True)
-    async def automod_default_ignores(self, ctx: GuildContext, *entities: IgnorableEntities):
-        """Specifies what roles, members, or channels will be ignored by AutoMod by default."""
-        try:
-            config = await self.bot.api.get_guild_automod_config(ctx.guild.id)
-        except NotFound:
-            config = {'default_ignores': []}
-
-        config['default_ignores'].extend(e.id for e in entities if e.id not in config['default_ignores'])
-
-        await self.bot.api.bulk_upsert_guild_automod_default_ignores(ctx.guild.id, config['default_ignores'])
-        await ctx.send(f"Now ignoring {', '.join([e.mention for e in entities])}")
-        await self.invalidate_config(ctx, config_name="guild_automod")
-
-    @automod.command(level=CommandLevel.Admin, name='unignore', require_var_positional=True)
-    @has_guild_permissions(manage_guild=True)
-    async def automod_default_unignore(self, ctx: GuildContext, *entities: IgnorableEntities) -> None:
-        """Specify roles, members, or channels to remove from AutoMod default ignores."""
-        try:
-            config = await self.bot.api.get_guild_automod_config(ctx.guild.id)
-        except NotFound:
-            await ctx.send("You have not set up any ignores!")
-            return
-
-        ignores: List[int] = config['default_ignores']
-        if not ignores:
-            await ctx.send("You have not set up any ignores!")
-            return
-
-        for entity in entities:
-            if entity.id in ignores:
-                ignores.remove(entity.id)
-
-        await self.bot.api.bulk_upsert_guild_automod_default_ignores(ctx.guild.id, ignores)
-        await ctx.send(f"Removed {', '.join(e.mention for e in entities)} from default ignores")
-        await self.invalidate_config(ctx, config_name="guild_automod")
-
-    @automod.command(level=CommandLevel.Admin, name='ignored')
-    @has_guild_permissions(manage_guild=True)
-    async def automod_ignored(self, ctx: GuildContext):
-        """Shows what roles, members, or channels are ignored by AutoMod"""
-        try:
-            config = await self.bot.api.get_guild_automod_config(ctx.guild.id)
-        except NotFound:
-            config = {'default_ignores': []}
-
-        # levels: Optional[LevelConfig] = attrgetter('permissions.levels')(await self.bot.get_guild_bot_config())
-        levels = None
-
-        if not config['default_ignores'] and not levels:
-            await ctx.send("You have no ignores set up!")
-            return
-
-        def resolve_snowflake(id: int):
-            if g := ctx.guild.get_channel_or_thread(id):
-                return g.mention
-            return g.mention if (g := ctx.guild.get_role(id)) else f"<@!{id}>"
-
-        pages = Paginator(ui.AutoModIgnoredPages([resolve_snowflake(g) for g in config['default_ignores']],
-                          per_page=10), context=ctx)
-        await pages.start()
-
-    @automod.group(level=CommandLevel.Admin, name='rules')
-    @has_guild_permissions(manage_guild=True)
-    async def automod_rules(self, ctx: GuildContext):
-        ...
-
-    @automod_rules.command(level=CommandLevel.Admin, name="add")
-    @has_guild_permissions(manage_guild=True)
-    async def add_automod_rules(self, ctx: GuildContext, type: AUTOMOD_EVENT_NAMES_LITERAL,
-                                *, count: str):
-        """Adds a new rule to automod.
-
-        You can provide count in the following ways
-        To set automod to do something at 5 messages per 10 seconds, you can express it in one of the following ways
-        - "5/10s"
-        - "5 10"
-        """
-        if type == "mass-mentions":
-            try:
-                result = AutoModDurationResponse(int(count), 0)
-            except ValueError:
-                await ctx.send("Could not convert to an integer")
-                return
-        else:
-            result: AutoModDurationResponse = await AutoModDuration().convert(ctx, count)
-
-        punishment = await ui.prompt_for_automod_punishments(ctx)
-        if punishment is None:
-            return
-
-        punishment_payload: Dict[str, Any] = {"type": punishment[0]}
-
-        # Discord removed selects so blame them for this
-        if punishment[0] in ("BAN", "MUTE"):
-            darg = await ctx.confirm("This punishment supports temporary actions!\nWould you like to set a duration for"
-                                     " this rule?")
-            if darg:
-                m = await ctx.ask("What would you like the duration to be?")
-                if not m:
-                    return
-
-                duration = ShortTime(m.content)
-                punishment_payload['duration'] = duration.delta.seconds
-
-        payload = {"guild_id": ctx.guild.id,
-                   "type": type,
-                   "count": result.count,
-                   "seconds": result.seconds,
-                   "punishment": punishment_payload}
-        try:
-            await self.bot.api.create_guild_automod_rule(ctx.guild.id, payload)
-        except DataConflict:
-            await ctx.send("This rule has already been set up!\nIf you want to edit this rule, please remove it and"
-                           " then re-run this command again!")
-            return
-
-        await ctx.send(f"Successfully set up {AUTOMOD_EVENT_NAMES_MAPPING[type]}!")
-        await self.invalidate_config(ctx, config_name="guild_automod")
-
-    @automod_rules.command(level=CommandLevel.Admin, name="remove")
-    @has_guild_permissions(manage_guild=True)
-    async def remove_automod_rule(self, ctx: GuildContext, rule: AUTOMOD_EVENT_NAMES_LITERAL):
-        """Removes an existing automod rule"""
-        try:
-            await self.bot.api.delete_guild_automod_rule(ctx.guild.id, rule)
-        except NotFound:
-            await ctx.send(f"{AUTOMOD_EVENT_NAMES_MAPPING[rule]} was never set up!")
-            return
-
-        await ctx.send(f"{AUTOMOD_EVENT_NAMES_MAPPING[rule]} was removed.")
-        await self.invalidate_config(ctx, config_name="guild_automod")
 
     # COMMAND OVERRIDES
 
