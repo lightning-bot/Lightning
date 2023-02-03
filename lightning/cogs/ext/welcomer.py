7f831669119b98012fc31056d630b7e9d374714b
LightSage <lightsage01@gmail.com>
2023-02-03 09:02:51 -0600

    Add support for adding join messages

--- lightning/cogs/ext/welcomer.py
+++ lightning/cogs/ext/welcomer.py
@@ -48,8 +48,8 @@ def to_template(text, *, guild: discord.Guild, user: discord.Member):
 
 
 class LeaveMessageModal(discord.ui.Modal):
-    def __init__(self, channel) -> None:
-        super().__init__(title="Set leave message")
+    def __init__(self, channel: discord.TextChannel) -> None:
+        super().__init__(title="Set Leave Message")
         self.channel = channel
 
     message = discord.ui.TextInput(label="Message", style=discord.TextStyle.paragraph, max_length=2040)
@@ -66,6 +66,27 @@ class LeaveMessageModal(discord.ui.Modal):
 
         example = to_template(self.message.value, guild=interaction.guild, user=interaction.user)
         await interaction.response.send_message(f"Set your leave message! Here's a preview:\n\n{example}")
+
+
+class JoinMessageModal(discord.ui.Modal):
+    def __init__(self, channel: discord.TextChannel) -> None:
+        super().__init__(title="Set Join Message")
+        self.channel = channel
+
+    message = discord.ui.TextInput(label="Message", style=discord.TextStyle.paragraph, max_length=2040)
+
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        query = """INSERT INTO welcomer (guild_id, join_message, join_channel)
+                   VALUES ($1, $2, $3)
+                   ON CONFLICT (guild_id)
+                   DO UPDATE SET
+                    join_message = EXCLUDED.join_message,
+                    join_channel = EXCLUDED.join_channel
+                   """
+        await interaction.client.pool.execute(query, interaction.guild.id, self.message.value, self.channel.id)
+
+        example = to_template(self.message.value, guild=interaction.guild, user=interaction.user)
+        await interaction.response.send_message(f"Set your join message! Here's a preview:\n\n{example}")
 
 
 @app_commands.default_permissions(manage_guild=True)
@@ -86,6 +107,13 @@ class Welcomer(GroupCog, group_name="welcomer"):
         modal = LeaveMessageModal(channel)
         await interaction.response.send_modal(modal)
 
+    @app_commands.command(name="set-join-message")
+    @app_commands.describe(channel="The channel to send join messages to")
+    async def set_join_message(self, interaction: discord.Interaction, channel: discord.TextChannel):
+        """Sets the server's join message"""
+        modal = JoinMessageModal(channel)
+        await interaction.response.send_modal(modal)
+
     async def get_guild_welcomer(self, guild_id: int):
         return await self.bot.pool.fetchrow("SELECT * FROM welcomer WHERE guild_id=$1;", guild_id)
 
@@ -103,6 +131,22 @@ class Welcomer(GroupCog, group_name="welcomer"):
             return
 
         text = to_template(record['leave_message'], guild=member.guild, user=member)
+        await channel.send(text, allowed_mentions=discord.AllowedMentions(everyone=False, roles=False, users=[member]))
+
+    @GroupCog.listener()
+    async def on_member_join(self, member: discord.Member):
+        if member.guild.id not in WHITELISTED_GUILDS:
+            return
+
+        record = await self.get_guild_welcomer(member.guild.id)
+        if not record:
+            return
+
+        channel = member.guild.get_channel(record['join_channel'])
+        if not channel:
+            return
+
+        text = to_template(record['join_message'], guild=member.guild, user=member)
         await channel.send(text, allowed_mentions=discord.AllowedMentions(everyone=False, roles=False, users=[member]))
 
 
