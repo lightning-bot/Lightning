aec5a47fd0ae97dc3f2fb786a6624c7c16181fc6
LightSage <lightsage01@gmail.com>
2023-04-25 08:26:01 -0500

    Require kick_members to use View Infractions

--- lightning/cogs/infractions.py
+++ lightning/cogs/infractions.py
@@ -173,6 +173,7 @@ class Infractions(LightningCog, required=['Moderation']):
 
     # Context menu
     @app_commands.guild_only()
+    @app_commands.default_permissions(kick_members=True)
     @app_commands.checks.has_permissions(kick_members=True)
     async def infraction_list(self, interaction: discord.Interaction, member: discord.Member):
         try:
