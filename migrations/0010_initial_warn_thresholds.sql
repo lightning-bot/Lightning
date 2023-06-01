a6f3a5d62a1bc82771bcf02e0f2f6f8101fe8ca5
LightSage <lightsage01@gmail.com>
2023-06-01 16:52:08 -0500

    Use automod_punishment_enum instead of just text

--- migrations/0010_initial_warn_thresholds.sql
+++ migrations/0010_initial_warn_thresholds.sql
@@ -1,4 +1,4 @@
 ALTER TABLE guild_automod_config ADD COLUMN IF NOT EXISTS warn_threshold SMALLINT;
-ALTER TABLE guild_automod_config ADD COLUMN IF NOT EXISTS warn_punishment TEXT;
+ALTER TABLE guild_automod_config ADD COLUMN IF NOT EXISTS warn_punishment automod_punishment_enum;
 
 -- In v5, we'll remove warn_kick and warn_ban
