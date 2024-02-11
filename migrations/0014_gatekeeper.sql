fc4e5202d97d5c875e389c1580155ad51bbe007d
LightSage <lightsage01@gmail.com>
2024-02-11 17:02:41 -0600

    more wip

--- migrations/0014_gatekeeper.sql
+++ migrations/0014_gatekeeper.sql
@@ -9,5 +9,7 @@ CREATE TABLE IF NOT EXISTS guild_gatekeeper_config
 
 CREATE TABLE IF NOT EXISTS pending_gatekeeper_members
 (
-    guild_id BIGINT NOT NULL REFERENCES guilds (id) ON DELETE CASCADE PRIMARY KEY
+    guild_id BIGINT NOT NULL REFERENCES guilds (id) ON DELETE CASCADE PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    failed_rule_action automod_punishment_enum  -- should only be filled when a guild's AutoMod requests Verification 1st, then do <x> action
 );
