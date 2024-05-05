c3f99fd061f6c96e06c60ea99f81e87c4a86bd69
LightSage <lightsage01@gmail.com>
2024-05-04 20:28:20 -0500

    Bump to 4.8.1

--- lightning/meta.py
+++ lightning/meta.py
@@ -21,6 +21,6 @@ __all__ = ('__title__',
            '__author__')
 
 __title__ = "Lightning"
-__version__ = "4.8.1a"
+__version__ = "4.8.1"
 __license__ = "GNU Affero General Public License Version 3"
 __author__ = "LightSage"
